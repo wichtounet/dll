6c0d2682f47ea24b851ff5dfb71791073f820a3a
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-07 16:17:11 +0100

    Prepare source file for RBM

---
+++ include/rbm.hpp
@@ -0,0 +1,22 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2014.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef DBN_RBM_HPP
+#define DBN_RBM_HPP
+
+namespace dbn {
+
+/*!
+ * \brief Restricted Boltzmann Machine
+ */
+struct rbm {
+
+};
+
+} //end of dbn namespace
+
+#endif
