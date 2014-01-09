9715f2d455534930d7299a7db9e5e68a44f55784
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-09 09:03:53 +0100

    Prepare

--- include/rbm.hpp
+++ include/rbm.hpp
@@ -13,18 +13,48 @@ namespace dbn {
 /*!
  * \brief Restricted Boltzmann Machine
  */
-template<typename Visible, typename Hidden, typename Bias, typename Weight = double>
+template<typename Visible, typename Hidden, typename Weight = double>
 struct rbm {
-    Visible[] visibles;
-    Hidden[] hiddens;
-    Bias bias;
+    std::vector<Visible> visibles;
+    std::vector<Hidden> hiddens;
+
     Weight[][] weights;
+    Weight[] bias_visibles;
+    Weight[] bias_hidden;
 
     std::size_t num_hidden;
     std::size_t num_visible;
 
     double learning_rate;
+
+    void epoch(TrainingItem[] item){
+        //Size should match
+
+        //Set the states of the visible units
+        for(size_t v = 0; v < num_visible; ++v){
+            visibles[v] = item[v];
+        }
+
+        //Update the states of the hidden units
+        auto energy_vh = - mul_sum(bias_hidden, visibles);
+
+
+
+
+
+    }
 };
+
+template<typename A, typename B>
+double mul_sum(const std::vector<A>& a, const std::vector<B>& b){
+    double acc = 0;
+
+    for(size_t i = 0; i < a.size(); ++i){
+        acc += a[i] * b[i];
+    }
+
+    return acc;
+}
 
 } //end of dbn namespace
 
