3bc08325fb8404946be42c1c614dc4ee565334ae
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-17 14:54:38 +0100

    Clean

--- src/rbm_mnist.cpp
+++ src/rbm_mnist.cpp
@@ -12,17 +12,13 @@
 #include "image_utils.hpp"
 
 int main(){
-    dbn::rbm<uint8_t, uint8_t, 50, true> rbm(28 * 28, 60);
+    dbn::rbm<uint8_t, uint8_t, 50, true> rbm(28 * 28, 36);
 
     auto training_images = mnist::read_training_images();
 
     binarize_each(training_images);
 
     rbm.train(training_images, 100);
-
-    rbm.display_weights(28);
-
-    //TODO
 
     return 0;
 }
