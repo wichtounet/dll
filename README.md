5581e4532da4e8a2a71f24519a4eda41c0ba7b1e
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-23 14:07:05 +0100

    Update license to MIT

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 dbn
 ===
+
+Distributed under the terms of the MIT license, see `LICENSE` file for details.
--- src/rbm_mnist.cpp
+++ src/rbm_mnist.cpp
@@ -1,8 +1,8 @@
 //=======================================================================
-// Copyright Baptiste Wicht 2014.
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
 //=======================================================================
 
 #include <iostream>
