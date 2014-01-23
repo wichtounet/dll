db6c771ec37cf488e8c4ad5f60e9ba12c9f7744d
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-01-23 12:40:45 +0100

    Cleanup

--- src/rbm_mnist.cpp
+++ src/rbm_mnist.cpp
@@ -11,7 +11,17 @@
 #include "mnist_reader.hpp"
 #include "image_utils.hpp"
 
-int main(){
+int main(int argc, char* argv[]){
+    auto reconstruction = false;
+
+    if(argc > 1){
+        std::string command(argv[1]);
+
+        if(command == "sample"){
+            reconstruction = true;
+        }
+    }
+
     dbn::rbm<50, true> rbm(28 * 28, 100);
 
     auto training_images = mnist::read_training_images();
@@ -20,21 +30,23 @@ int main(){
 
     rbm.train(training_images, 5);
 
-    for(size_t t = 0; t < 10; ++t){
-        auto& image = training_images[666 * t];
+    if(reconstruction){
+        for(size_t t = 0; t < 10; ++t){
+            auto& image = training_images[666 * t];
 
-        std::cout << "Source image" << std::endl;
-        for(size_t i = 0; i < 28; ++i){
-            for(size_t j = 0; j < 28; ++j){
-                std::cout << static_cast<size_t>(image[i * 28 + j]) << " ";
+            std::cout << "Source image" << std::endl;
+            for(size_t i = 0; i < 28; ++i){
+                for(size_t j = 0; j < 28; ++j){
+                    std::cout << static_cast<size_t>(image[i * 28 + j]) << " ";
+                }
+                std::cout << std::endl;
             }
-            std::cout << std::endl;
+
+            rbm.reconstruct(image);
+
+            std::cout << "Reconstructed image" << std::endl;
+            rbm.display_visible_units(28);
         }
-
-        rbm.reconstruct(image);
-
-        std::cout << "Reconstructed image" << std::endl;
-        rbm.display_visible_units(28);
     }
 
     return 0;
