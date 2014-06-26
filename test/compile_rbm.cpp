30b0f9a0f20fc55e357559dc7b573b7f5d211db2
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-26 14:18:01 +0200

    More tests

--- test/compile_rbm.cpp
+++ test/compile_rbm.cpp
@@ -8,6 +8,9 @@ void test_rbm(){
     rbm.train(training, 10);
 }
 
+template <typename RBM>
+using pcd2_trainer_t = dbn::persistent_cd_trainer<2, RBM>;
+
 int main(){
     //Very basic RBM that must compile
     typedef dbn::rbm<dbn::layer<dbn::conf<true, 50, true, false, dbn::DecayType::L2, false, dbn::Type::SIGMOID, dbn::Type::SIGMOID>, 100, 100>> rbm_1;
@@ -18,9 +21,21 @@ int main(){
     //Sparsity
     typedef dbn::rbm<dbn::layer<dbn::conf<true, 50, true, false, dbn::DecayType::L2, true, dbn::Type::SIGMOID, dbn::Type::SIGMOID>, 100, 100>> rbm_3;
 
+    //PCD-2
+
+    typedef dbn::rbm<dbn::layer<dbn::conf<true, 50, true, false, dbn::DecayType::NONE, false, dbn::Type::GAUSSIAN, dbn::Type::NRLU, pcd2_trainer_t>, 100, 100>> rbm_4;
+
+    //PCD-2 and sparsity
+
+    typedef dbn::rbm<dbn::layer<dbn::conf<true, 50, true, false, dbn::DecayType::NONE, true, dbn::Type::GAUSSIAN, dbn::Type::SIGMOID, pcd2_trainer_t>, 100, 100>> rbm_5;
+
+    //Test them all
+
     test_rbm<rbm_1>();
     test_rbm<rbm_2>();
     test_rbm<rbm_3>();
+    test_rbm<rbm_4>();
+    test_rbm<rbm_5>();
 
     return 0;
 }
