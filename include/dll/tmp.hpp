a03c8ccb14c052a110e4dccf4e02eb08db963303
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-02 14:46:01 +0200

    Remove unused code for now

--- include/dll/tmp.hpp
+++ include/dll/tmp.hpp
@@ -135,24 +135,6 @@ struct get_template_type<D> {
     using type = typename D::template value<RBM>;
 };
 
-static constexpr std::size_t ct_mid(std::size_t a, std::size_t b){
-    return (a+b) / 2;
-}
-
-static constexpr std::size_t ct_pow(std::size_t a){
-    return a*a;
-}
-
-static constexpr std::size_t ct_sqrt(std::size_t res, std::size_t l, std::size_t r){
-    return
-        l == r ? r
-        : ct_sqrt(res, ct_pow(ct_mid(r, l)) >= res ? l : ct_mid(r, l) + 1, ct_pow(ct_mid(r, l)) >= res ? ct_mid(r, l) : r);
-}
-
-static constexpr std::size_t ct_sqrt(std::size_t res){
-    return ct_sqrt(res, 1, res);
-}
-
 } //end of dbn namespace
 
 #endif
