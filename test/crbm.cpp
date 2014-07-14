70926f0def284c863a9f6b1c2c89ebf570635b38
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-14 09:05:25 +0200

    New test cases

--- test/crbm.cpp
+++ test/crbm.cpp
@@ -44,3 +44,43 @@ TEST_CASE( "crbm/mnist_2", "crbm::momentum" ) {
 
     REQUIRE(error < 1e-2);
 }
+
+TEST_CASE( "crbm/mnist_3", "crbm::decay_l1" ) {
+    dll::conv_layer<
+        28, 12, 40,
+        dll::batch_size<25>,
+        dll::weight_decay<dll::decay_type::L1_FULL>
+    >::rbm_t rbm;
+
+    auto dataset = mnist::read_dataset<std::vector, vector, double>();
+
+    REQUIRE(!dataset.training_images.empty());
+    dataset.training_images.resize(100);
+
+    mnist::binarize_dataset(dataset);
+
+    rbm.learning_rate = 0.001;
+    auto error = rbm.train(dataset.training_images, 100);
+
+    REQUIRE(error < 1e-2);
+}
+
+TEST_CASE( "crbm/mnist_4", "crbm::decay_l2" ) {
+    dll::conv_layer<
+        28, 12, 40,
+        dll::batch_size<25>,
+        dll::weight_decay<dll::decay_type::L2_FULL>
+    >::rbm_t rbm;
+
+    auto dataset = mnist::read_dataset<std::vector, vector, double>();
+
+    REQUIRE(!dataset.training_images.empty());
+    dataset.training_images.resize(100);
+
+    mnist::binarize_dataset(dataset);
+
+    rbm.learning_rate = 0.001;
+    auto error = rbm.train(dataset.training_images, 100);
+
+    REQUIRE(error < 1e-2);
+}
--- tests.mk
+++ tests.mk
@@ -30,6 +30,30 @@ release_test_crbm_mp/mnist_1 : release_dll_test
 	 @ echo "Run crbm_mp/mnist_1" > test_reports/test_crbm_mp-mnist_1.log
 	 @ ./release/bin/dll_test crbm_mp/mnist_1 >> test_reports/test_crbm_mp-mnist_1.log
 
+debug_test_crbm_mp/mnist_2 : debug_dll_test
+	 @ echo "Run crbm_mp/mnist_2" > test_reports/test_crbm_mp-mnist_2.log
+	 @ ./debug/bin/dll_test crbm_mp/mnist_2 >> test_reports/test_crbm_mp-mnist_2.log
+
+release_test_crbm_mp/mnist_2 : release_dll_test
+	 @ echo "Run crbm_mp/mnist_2" > test_reports/test_crbm_mp-mnist_2.log
+	 @ ./release/bin/dll_test crbm_mp/mnist_2 >> test_reports/test_crbm_mp-mnist_2.log
+
+debug_test_crbm_mp/mnist_3 : debug_dll_test
+	 @ echo "Run crbm_mp/mnist_3" > test_reports/test_crbm_mp-mnist_3.log
+	 @ ./debug/bin/dll_test crbm_mp/mnist_3 >> test_reports/test_crbm_mp-mnist_3.log
+
+release_test_crbm_mp/mnist_3 : release_dll_test
+	 @ echo "Run crbm_mp/mnist_3" > test_reports/test_crbm_mp-mnist_3.log
+	 @ ./release/bin/dll_test crbm_mp/mnist_3 >> test_reports/test_crbm_mp-mnist_3.log
+
+debug_test_crbm_mp/mnist_4 : debug_dll_test
+	 @ echo "Run crbm_mp/mnist_4" > test_reports/test_crbm_mp-mnist_4.log
+	 @ ./debug/bin/dll_test crbm_mp/mnist_4 >> test_reports/test_crbm_mp-mnist_4.log
+
+release_test_crbm_mp/mnist_4 : release_dll_test
+	 @ echo "Run crbm_mp/mnist_4" > test_reports/test_crbm_mp-mnist_4.log
+	 @ ./release/bin/dll_test crbm_mp/mnist_4 >> test_reports/test_crbm_mp-mnist_4.log
+
 debug_test_crbm/mnist_1 : debug_dll_test
 	 @ echo "Run crbm/mnist_1" > test_reports/test_crbm-mnist_1.log
 	 @ ./debug/bin/dll_test crbm/mnist_1 >> test_reports/test_crbm-mnist_1.log
@@ -38,10 +62,34 @@ release_test_crbm/mnist_1 : release_dll_test
 	 @ echo "Run crbm/mnist_1" > test_reports/test_crbm-mnist_1.log
 	 @ ./release/bin/dll_test crbm/mnist_1 >> test_reports/test_crbm-mnist_1.log
 
-debug_test_all: debug_test_rbm/mnist_1 debug_test_rbm/mnist_2 debug_test_rbm/mnist_3 debug_test_crbm_mp/mnist_1 debug_test_crbm/mnist_1 
+debug_test_crbm/mnist_2 : debug_dll_test
+	 @ echo "Run crbm/mnist_2" > test_reports/test_crbm-mnist_2.log
+	 @ ./debug/bin/dll_test crbm/mnist_2 >> test_reports/test_crbm-mnist_2.log
+
+release_test_crbm/mnist_2 : release_dll_test
+	 @ echo "Run crbm/mnist_2" > test_reports/test_crbm-mnist_2.log
+	 @ ./release/bin/dll_test crbm/mnist_2 >> test_reports/test_crbm-mnist_2.log
+
+debug_test_crbm/mnist_3 : debug_dll_test
+	 @ echo "Run crbm/mnist_3" > test_reports/test_crbm-mnist_3.log
+	 @ ./debug/bin/dll_test crbm/mnist_3 >> test_reports/test_crbm-mnist_3.log
+
+release_test_crbm/mnist_3 : release_dll_test
+	 @ echo "Run crbm/mnist_3" > test_reports/test_crbm-mnist_3.log
+	 @ ./release/bin/dll_test crbm/mnist_3 >> test_reports/test_crbm-mnist_3.log
+
+debug_test_crbm/mnist_4 : debug_dll_test
+	 @ echo "Run crbm/mnist_4" > test_reports/test_crbm-mnist_4.log
+	 @ ./debug/bin/dll_test crbm/mnist_4 >> test_reports/test_crbm-mnist_4.log
+
+release_test_crbm/mnist_4 : release_dll_test
+	 @ echo "Run crbm/mnist_4" > test_reports/test_crbm-mnist_4.log
+	 @ ./release/bin/dll_test crbm/mnist_4 >> test_reports/test_crbm-mnist_4.log
+
+debug_test_all: debug_test_rbm/mnist_1 debug_test_rbm/mnist_2 debug_test_rbm/mnist_3 debug_test_crbm_mp/mnist_1 debug_test_crbm_mp/mnist_2 debug_test_crbm_mp/mnist_3 debug_test_crbm_mp/mnist_4 debug_test_crbm/mnist_1 debug_test_crbm/mnist_2 debug_test_crbm/mnist_3 debug_test_crbm/mnist_4 
 	 @ bash ./tools/test_report.sh
 
-release_test_all: release_test_rbm/mnist_1 release_test_rbm/mnist_2 release_test_rbm/mnist_3 release_test_crbm_mp/mnist_1 release_test_crbm/mnist_1 
+release_test_all: release_test_rbm/mnist_1 release_test_rbm/mnist_2 release_test_rbm/mnist_3 release_test_crbm_mp/mnist_1 release_test_crbm_mp/mnist_2 release_test_crbm_mp/mnist_3 release_test_crbm_mp/mnist_4 release_test_crbm/mnist_1 release_test_crbm/mnist_2 release_test_crbm/mnist_3 release_test_crbm/mnist_4 
 	 @ bash ./tools/test_report.sh
 
-.PHONY: release_test_all debug_test_alldebug_test_rbm/mnist_1 release_test_rbm/mnist_1 debug_test_rbm/mnist_2 release_test_rbm/mnist_2 debug_test_rbm/mnist_3 release_test_rbm/mnist_3 debug_test_crbm_mp/mnist_1 release_test_crbm_mp/mnist_1 debug_test_crbm/mnist_1 release_test_crbm/mnist_1 
+.PHONY: release_test_all debug_test_alldebug_test_rbm/mnist_1 release_test_rbm/mnist_1 debug_test_rbm/mnist_2 release_test_rbm/mnist_2 debug_test_rbm/mnist_3 release_test_rbm/mnist_3 debug_test_crbm_mp/mnist_1 release_test_crbm_mp/mnist_1 debug_test_crbm_mp/mnist_2 release_test_crbm_mp/mnist_2 debug_test_crbm_mp/mnist_3 release_test_crbm_mp/mnist_3 debug_test_crbm_mp/mnist_4 release_test_crbm_mp/mnist_4 debug_test_crbm/mnist_1 release_test_crbm/mnist_1 debug_test_crbm/mnist_2 release_test_crbm/mnist_2 debug_test_crbm/mnist_3 release_test_crbm/mnist_3 debug_test_crbm/mnist_4 release_test_crbm/mnist_4 
