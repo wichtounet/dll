74ad32c685faf69bb4555cee97336dc4bcf13e45
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-16 09:21:48 +0200

    Doc

--- include/dll/decay_type.hpp
+++ include/dll/decay_type.hpp
@@ -10,12 +10,15 @@
 
 namespace dll {
 
+/*!
+ * \brief Define how weight decay is applied.
+ */
 enum class decay_type {
-    NONE,
-    L1,
-    L2,
-    L1_FULL,
-    L2_FULL
+    NONE,           ///< No weight decay is applied during training
+    L1,             ///< Apply L1 weight decay on weights
+    L2,             ///< Apply L2 weight decay on weights
+    L1_FULL,        ///< Apply L1 weight decay on weights and biases
+    L2_FULL         ///< Apply L2 weight decay on weights and biases
 };
 
 } //end of dbn namespace
