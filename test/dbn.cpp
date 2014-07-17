48d712c8c436afea473d2c7cfc386f6db0421496
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-17 21:51:06 +0200

    Add copyrights

--- test/dbn.cpp
+++ test/dbn.cpp
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
 #include <deque>
 
 #include "catch.hpp"
--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,9 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
