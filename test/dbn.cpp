14e5589f4dffb931326f9d8fe5d8605228b232ec
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-17 22:19:16 +0200

    Add test case with labels

--- test/dbn.cpp
+++ test/dbn.cpp
@@ -58,3 +58,25 @@ TEST_CASE( "dbn/mnist_2", "dbn::containers" ) {
 
     REQUIRE(error < 5e-2);
 }
+
+TEST_CASE( "dbn/mnist_3", "dbn::labels" ) {
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>();
+
+    REQUIRE(!dataset.training_images.empty());
+    dataset.training_images.resize(1000);
+    dataset.training_labels.resize(1000);
+
+    mnist::binarize_dataset(dataset);
+
+    typedef dll::dbn<
+        dll::layer<28 * 28, 200, dll::in_dbn, dll::batch_size<50>, dll::init_weights, dll::momentum>,
+        dll::layer<200, 300, dll::in_dbn, dll::batch_size<50>, dll::momentum>,
+        dll::layer<310, 500, dll::in_dbn, dll::batch_size<50>, dll::momentum>> dbn_simple_t;
+
+    auto dbn = make_unique<dbn_simple_t>();
+
+    dbn->train_with_labels(dataset.training_images, dataset.training_labels, 10, 10);
+
+    auto error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::label_predictor());
+    REQUIRE(error < 0.3);
+}
