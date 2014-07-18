5f3199895019abbc6203c1ee6a482e8e69730b3c
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-18 14:15:46 +0200

    Add test case for exp unit

--- test/rbm.cpp
+++ test/rbm.cpp
@@ -247,3 +247,26 @@ TEST_CASE( "rbm/mnist_12", "rbm::init_weights" ) {
 
     REQUIRE(error < 1e-3);
 }
+
+TEST_CASE( "rbm/mnist_13", "rbm::exp" ) {
+    dll::layer<
+        28 * 28, 100,
+       dll::batch_size<25>,
+       dll::hidden<dll::unit_type::EXP>
+    >::rbm_t rbm;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>();
+
+    REQUIRE(!dataset.training_images.empty());
+    dataset.training_images.resize(100);
+
+    mnist::binarize_dataset(dataset);
+
+    auto error = rbm.train(dataset.training_images, 100);
+
+    //This test is kind of fake since exp unit are not really made for
+    //reconstruction. It is here to ensure that exp units are working.
+    //exponential units are not even made for training
+
+    REQUIRE(error < 1e-2);
+}
