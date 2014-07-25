7255dc4ce4a259f4e0bab9d709f7d1ba8ed133f4
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-25 20:28:05 +0200

    Prepare placeholder for layers

---
+++ include/dll/dbn_layers.hpp
@@ -0,0 +1,25 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef DLL_DBN_LAYERS_HPP
+#define DLL_DBN_LAYERS_HPP
+
+namespace dll {
+
+/**
+ * \brief Simple placeholder for a collection of layers
+ */
+template<typename... Layers>
+struct dbn_layers {
+    static constexpr const std::size_t layers = sizeof...(Layers);
+
+    static_assert(layers > 0, "A DBN must have at least 1 layer");
+};
+
+} //end of namespace dll
+
+#endif
