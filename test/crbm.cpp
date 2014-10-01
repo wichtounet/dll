898b3611af8e19538985c6f43b05366556ead19d
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-01 13:16:38 +0200

    Prepare tests

--- test/crbm.cpp
+++ test/crbm.cpp
@@ -87,12 +87,16 @@ TEST_CASE( "crbm/mnist_4", "crbm::decay_l2" ) {
     REQUIRE(error < 1e-2);
 }
 
-TEST_CASE( "crbm/mnist_5", "crbm::sparsity" ) {
-    dll::conv_rbm_desc<
+TEST_CASE( "crbm/mnist_60", "crbm::global_sparsity" ) {
+    using rbm_type = dll::conv_rbm_desc<
         28, 12, 40,
         dll::batch_size<25>,
         dll::sparsity<>
-    >::rbm_t rbm;
+    >::rbm_t;
+
+    REQUIRE(dll::rbm_traits<rbm_type>::sparsity_method() == dll::sparsity_method::GLOBAL_TARGET);
+
+    rbm_type rbm;
 
     //0.01 (default) is way too low for few hidden units
     rbm.sparsity_target = 0.1;
@@ -107,7 +111,32 @@ TEST_CASE( "crbm/mnist_5", "crbm::sparsity" ) {
 
     auto error = rbm.train(dataset.training_images, 100);
 
-    REQUIRE(error < 1e-1);
+    REQUIRE(error < 1e-2);
+}
+
+TEST_CASE( "crbm/mnist_61", "crbm::local_sparsity" ) {
+    using rbm_type = dll::conv_rbm_desc<
+        28, 12, 40,
+        dll::batch_size<25>,
+        dll::sparsity<dll::sparsity_method::LOCAL_TARGET>
+    >::rbm_t;
+
+    rbm_type rbm;
+
+    //0.01 (default) is way too low for few hidden units
+    rbm.sparsity_target = 0.1;
+    rbm.sparsity_cost = 0.9;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>();
+
+    REQUIRE(!dataset.training_images.empty());
+    dataset.training_images.resize(100);
+
+    mnist::binarize_dataset(dataset);
+
+    auto error = rbm.train(dataset.training_images, 100);
+
+    REQUIRE(error < 1e-2);
 }
 
 TEST_CASE( "crbm/mnist_6", "crbm::gaussian" ) {
