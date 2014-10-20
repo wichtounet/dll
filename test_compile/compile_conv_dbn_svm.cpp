194f877f91f762a4b57fa0b841fa33481a791cd6
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-20 14:02:37 +0200

    Reactivate test

--- test_compile/compile_conv_dbn_svm.cpp
+++ test_compile/compile_conv_dbn_svm.cpp
@@ -7,6 +7,7 @@
 
 #define DLL_SVM_SUPPORT
 
+#include "dll/conv_rbm.hpp"
 #include "dll/conv_dbn.hpp"
 
 template<typename DBN>
@@ -28,21 +29,17 @@ void test_dbn(){
     dbn.svm_predict(images[1]);
 }
 
-template <typename RBM>
-using pcd2_trainer_t = dll::persistent_cd_trainer<2, RBM>;
-
 int main(){
     //Basic example
 
-    //TODO Enable again once multi channel RBM are supported
-    //typedef dll::conv_dbn_desc<
-        //dll::dbn_layers<
-        //dll::conv_rbm_desc<28, 12, 40, dll::momentum, dll::batch_size<50>>::rbm_t,
-        //dll::conv_rbm_desc<12*12*40, 6, 40, dll::momentum, dll::batch_size<50>>::rbm_t>>::dbn_t dbn_1;
+    typedef dll::conv_dbn_desc<
+        dll::dbn_layers<
+        dll::conv_rbm_desc<28, 1, 12, 40, dll::momentum, dll::batch_size<50>>::rbm_t,
+        dll::conv_rbm_desc<12, 40, 6, 40, dll::momentum, dll::batch_size<50>>::rbm_t>>::dbn_t dbn_1;
 
-    ////Test them all
+    //Test them all
 
-    //test_dbn<dbn_1>();
+    test_dbn<dbn_1>();
 
     return 0;
 }
