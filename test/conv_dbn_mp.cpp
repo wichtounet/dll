59f9652b221cfb91f52bba285c299cc1a5e18415
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-21 13:37:01 +0200

    New tests

--- test/conv_dbn_mp.cpp
+++ test/conv_dbn_mp.cpp
@@ -9,6 +9,8 @@
 
 #include "catch.hpp"
 
+#define DLL_SVM_SUPPORT
+
 #include "dll/conv_rbm_mp.hpp"
 #include "dll/conv_dbn.hpp"
 
@@ -22,15 +24,65 @@ TEST_CASE( "conv_dbn_mp/mnist_1", "conv_dbn::simple" ) {
         dll::conv_rbm_mp_desc<6, 40, 4, 20, 2, dll::momentum, dll::batch_size<25>>::rbm_t
     >>::dbn_t dbn_t;
 
-    auto dataset = mnist::read_dataset<std::vector, std::vector, double>();
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(100);
 
     REQUIRE(!dataset.training_images.empty());
-    dataset.training_images.resize(100);
-    dataset.training_labels.resize(100);
 
     mnist::binarize_dataset(dataset);
 
     auto dbn = std::make_unique<dbn_t>();
 
     dbn->pretrain(dataset.training_images, 5);
+}
+
+TEST_CASE( "conv_dbn_mp/mnist_2", "conv_dbn::svm_simple" ) {
+    typedef dll::conv_dbn_desc<
+        dll::dbn_layers<
+        dll::conv_rbm_mp_desc<28, 1, 18, 40, 2, dll::momentum, dll::batch_size<25>>::rbm_t,
+        dll::conv_rbm_mp_desc<9, 40, 6, 40, 2, dll::momentum, dll::batch_size<25>>::rbm_t
+    >>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(200);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
+
+TEST_CASE( "conv_dbn_mp/mnist_3", "conv_dbn::svm_concatenate" ) {
+    typedef dll::conv_dbn_desc<
+        dll::dbn_layers<
+        dll::conv_rbm_mp_desc<28, 1, 18, 40, 2, dll::momentum, dll::batch_size<25>>::rbm_t,
+        dll::conv_rbm_mp_desc<9, 40, 6, 40, 2, dll::momentum, dll::batch_size<25>>::rbm_t
+    >, dll::concatenate>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(200);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
 }
