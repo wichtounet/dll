81f8f49679d158e4951dd2b40c4258cdae0bab72
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-10-21 13:05:23 +0200

    New tests

--- test/conv_dbn.cpp
+++ test/conv_dbn.cpp
@@ -9,6 +9,8 @@
 
 #include "catch.hpp"
 
+#define DLL_SVM_SUPPORT
+
 #include "dll/conv_rbm.hpp"
 #include "dll/conv_dbn.hpp"
 
@@ -33,4 +35,54 @@ TEST_CASE( "conv_dbn/mnist_1", "conv_dbn::simple" ) {
     auto dbn = std::make_unique<dbn_t>();
 
     dbn->pretrain(dataset.training_images, 5);
+}
+
+TEST_CASE( "conv_dbn/mnist_2", "conv_dbn::svm_simple" ) {
+    typedef dll::conv_dbn_desc<
+        dll::dbn_layers<
+        dll::conv_rbm_desc<28, 1, 12, 40, dll::momentum, dll::batch_size<25>>::rbm_t,
+        dll::conv_rbm_desc<12, 40, 10, 40, dll::momentum, dll::batch_size<25>>::rbm_t>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(200);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.1);
+}
+
+TEST_CASE( "conv_dbn/mnist_3", "conv_dbn::svm_concatenate" ) {
+    typedef dll::conv_dbn_desc<
+        dll::dbn_layers<
+        dll::conv_rbm_desc<28, 1, 12, 40, dll::momentum, dll::batch_size<25>>::rbm_t,
+        dll::conv_rbm_desc<12, 40, 10, 40, dll::momentum, dll::batch_size<25>>::rbm_t>, dll::concatenate>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(200);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.1);
 }
