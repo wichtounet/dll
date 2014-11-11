0c2b0d7fdaaa7520aa5329fc39bc48b66c301420
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-11-11 14:24:52 +0100

    Escalate members

--- include/dll/standard_conv_rbm.hpp
+++ include/dll/standard_conv_rbm.hpp
@@ -37,6 +37,8 @@ public:
 
 public:
 
+    //Constructors
+
     standard_conv_rbm(){
         //Note: Convolutional RBM needs lower learning rate than standard RBM
 
@@ -47,13 +49,7 @@ public:
             :   /* Only Gaussian Units needs lower rate */         1e-3;
     }
 
-    void store(std::ostream& os) const {
-        store(os, *static_cast<parent_t*>(this));
-    }
-
-    void load(std::istream& is){
-        load(is, *static_cast<parent_t*>(this));
-    }
+    //Train functions
 
     template<typename Samples, bool EnableWatcher = true, typename RW = void, typename... Args>
     weight train(Samples& training_data, std::size_t max_epochs, Args... args){
@@ -82,7 +78,44 @@ public:
             max_epochs);
     }
 
+    //I/O functions
+
+    void store(std::ostream& os) const {
+        store(os, *static_cast<parent_t*>(this));
+    }
+
+    void load(std::istream& is){
+        load(is, *static_cast<parent_t*>(this));
+    }
+
+    //Utility functions
+
+    template<typename Sample>
+    void reconstruct(const Sample& items){
+        reconstruct(items, *static_cast<parent_t*>(this));
+    }
+
+    void display_visible_unit_activations() const {
+        display_visible_unit_activations(*static_cast<parent_t*>(this));
+    }
+
+    void display_visible_unit_samples() const {
+        display_visible_unit_samples(*static_cast<parent_t*>(this));
+    }
+
+    void display_hidden_unit_activations() const {
+        display_hidden_unit_samples(*static_cast<parent_t*>(this));
+    }
+
+    void display_hidden_unit_samples() const {
+        display_hidden_unit_samples(*static_cast<parent_t*>(this));
+    }
+
 private:
+
+    //Since the sub classes do not have the same fields, it is not possible
+    //to put the fields in standard_rbm, therefore, it is necessary to use template
+    //functions to implement the details
 
     template<typename RBM>
     static void store(std::ostream& os, const RBM& rbm){
@@ -92,12 +125,82 @@ private:
     }
 
     template<typename RBM>
-    void load(std::istream& is, RBM& rbm){
+    static void load(std::istream& is, RBM& rbm){
         binary_load_all(is, rbm.w);
         binary_load_all(is, rbm.b);
         binary_load(is, rbm.c);
     }
 
+    template<typename Sample, typename RBM>
+    static void reconstruct(const Sample& items, RBM& rbm){
+        cpp_assert(items.size() == RBM::input_size(), "The size of the training sample must match visible units");
+
+        cpp::stop_watch<> watch;
+
+        //Set the state of the visible units
+        rbm.v1 = items;
+
+        rbm.activate_hidden(rbm.h1_a, rbm.h1_s, rbm.v1, rbm.v1);
+
+        rbm.activate_visible(rbm.h1_a, rbm.h1_s, rbm.v2_a, rbm.v2_s);
+        rbm.activate_hidden(rbm.h2_a, rbm.h2_s, rbm.v2_a, rbm.v2_s);
+
+        std::cout << "Reconstruction took " << watch.elapsed() << "ms" << std::endl;
+    }
+
+    template<typename RBM>
+    static void display_visible_unit_activations(const RBM& rbm){
+        for(std::size_t channel = 0; channel < RBM::NC; ++channel){
+            std::cout << "Channel " << channel << std::endl;
+
+            for(size_t i = 0; i < RBM::NV; ++i){
+                for(size_t j = 0; j < RBM::NV; ++j){
+                    std::cout << rbm.v2_a(channel, i, j) << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+
+    template<typename RBM>
+    static void display_visible_unit_samples(const RBM& rbm){
+        for(std::size_t channel = 0; channel < RBM::NC; ++channel){
+            std::cout << "Channel " << channel << std::endl;
+
+            for(size_t i = 0; i < RBM::NV; ++i){
+                for(size_t j = 0; j < RBM::NV; ++j){
+                    std::cout << rbm.v2_s(channel, i, j) << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+    }
+
+    template<typename RBM>
+    static void display_hidden_unit_activations(const RBM& rbm){
+        for(size_t k = 0; k < RBM::K; ++k){
+            for(size_t i = 0; i < RBM::NV; ++i){
+                for(size_t j = 0; j < RBM::NV; ++j){
+                    std::cout << rbm.h2_a(k)(i, j) << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl << std::endl;
+        }
+    }
+
+    template<typename RBM>
+    static void display_hidden_unit_samples(const RBM& rbm){
+        for(size_t k = 0; k < RBM::K; ++k){
+            for(size_t i = 0; i < RBM::NV; ++i){
+                for(size_t j = 0; j < RBM::NV; ++j){
+                    std::cout << rbm.h2_s(k)(i, j) << " ";
+                }
+                std::cout << std::endl;
+            }
+            std::cout << std::endl << std::endl;
+        }
+    }
 };
 
 } //end of dll namespace
