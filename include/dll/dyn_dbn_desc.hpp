a697c0b166b40348f568c5e9a2ddbb0fe84261ee
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-11-25 13:42:45 +0100

    Prepare dyn_dbn

---
+++ include/dll/dyn_dbn_desc.hpp
@@ -0,0 +1,54 @@
+//=======================================================================
+// Copyright (c) 2014 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef DLL_DYN_DBN_DESC_HPP
+#define DLL_DYN_DBN_DESC_HPP
+
+#include "base_conf.hpp"
+#include "conjugate_gradient.hpp"
+#include "watcher.hpp"
+#include "tmp.hpp"
+
+namespace dll {
+
+template <typename DBN>
+using default_dyn_dbn_trainer_t = cg_trainer<DBN, false>;
+
+/*!
+ * \brief Describe a Dynamic DBN *
+ *
+ * A "dynamic" can be configured at runtime rather than at compile-time.
+ *
+ * This struct should be used to define a DBN.
+ * Once configured, the ::dbn_t member returns the type of the configured DBN.
+ */
+template<typename... Parameters>
+struct dyn_dbn_desc {
+    static constexpr const bool Momentum = detail::is_present<momentum, Parameters...>::value;
+    static constexpr const decay_type Decay = detail::get_value<weight_decay<decay_type::NONE>, Parameters...>::value;
+    static constexpr const bool Concatenate = detail::is_present<concatenate, Parameters...>::value;
+
+    /*! The type of the trainer to use to train the DBN */
+    template <typename DBN>
+    using trainer_t = typename detail::get_template_type<trainer<default_dbn_trainer_t>, Parameters...>::template type<DBN>;
+
+    /*! The type of the watched to use during training */
+    template <typename DBN>
+    using watcher_t = typename detail::get_template_type<watcher<default_dbn_watcher>, Parameters...>::template type<DBN>;
+
+    /*! The DBN type */
+    using dbn_t = dbn<dyn_dbn_desc<Layers, Parameters...>>;
+
+    //Make sure only valid types are passed to the configuration list
+    static_assert(
+        detail::is_valid<detail::tmp_list<trainer_id, watcher_id, momentum_id, weight_decay_id, concatenate_id>, Parameters...>::value,
+        "Invalid parameters type");
+};
+
+} //end of dll namespace
+
+#endif
--- test/dyn_dbn.cpp
+++ test/dyn_dbn.cpp
@@ -12,8 +12,12 @@
 #define DLL_SVM_SUPPORT
 
 #include "dll/dyn_rbm.hpp"
+
 #include "dll/dbn.hpp"
 #include "dll/dbn_desc.hpp"
+
+#include "dll/dyn_dbn_desc.hpp"
+
 #include "dll/dbn_layers.hpp"
 #include "dll/stochastic_gradient_descent.hpp"
 
