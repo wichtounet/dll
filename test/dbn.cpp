a55869f6dd8fbd8a807b4cf18c5a94cb65889eea
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-02-09 15:46:47 +0100

    Add a faster test case

--- test/dbn.cpp
+++ test/dbn.cpp
@@ -402,6 +402,33 @@ TEST_CASE( "dbn/mnist_15", "dbn::parallel" ) {
     REQUIRE(test_error < 0.2);
 }
 
+TEST_CASE( "dbn/mnist_16", "dbn::fast" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+        dll::rbm_desc<28 * 28, 100, dll::momentum, dll::batch_size<5>, dll::init_weights>::rbm_t,
+        dll::rbm_desc<100, 200, dll::momentum, dll::batch_size<5>>::rbm_t,
+        dll::rbm_desc<200, 10, dll::momentum, dll::batch_size<5>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(25);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 5);
+    auto error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 2, 5);
+
+    REQUIRE(error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+
+    std::cout << "test_error:" << test_error << std::endl;
+
+    REQUIRE(test_error < 0.2);
+}
+
 //{{{ Performance debugging tests
 
 TEST_CASE( "dbn/mnist_101", "dbn::slow_parallel" ) {
