7ae408e12d90600ea44875d4b2c811e317db5b0a
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-02-28 13:29:31 +0100

    Reduce duplication

--- include/dll/avgp_layer_desc.hpp
+++ include/dll/avgp_layer_desc.hpp
@@ -8,27 +8,14 @@
 #ifndef DLL_AVGP_LAYER_DESC_HPP
 #define DLL_AVGP_LAYER_DESC_HPP
 
+#include "pooling_layer_desc.hpp"
+
 namespace dll {
 
 template<std::size_t T_I1, std::size_t T_I2, std::size_t T_I3, std::size_t T_C1, std::size_t T_C2, std::size_t T_C3>
-struct avgp_layer_3d_desc {
-    static constexpr const std::size_t I1 = T_I1;
-    static constexpr const std::size_t I2 = T_I2;
-    static constexpr const std::size_t I3 = T_I3;
-    static constexpr const std::size_t C1 = T_C1;
-    static constexpr const std::size_t C2 = T_C2;
-    static constexpr const std::size_t C3 = T_C3;
-
-    static_assert(C1 > 0, "Cannot shrink a layer by less than 1");
-    static_assert(C2 > 0, "Cannot shrink a layer by less than 1");
-    static_assert(C3 > 0, "Cannot shrink a layer by less than 1");
-    static_assert(C1 + C2 + C3 > 3, "If neither C is > 1, the layer has no effect");
-    static_assert(I1 % C1 == 0, "Input dimension is not divisible by C");
-    static_assert(I2 % C2 == 0, "Input dimension is not divisible by C");
-    static_assert(I3 % C3 == 0, "Input dimension is not divisible by C");
-
+struct avgp_layer_3d_desc : pooling_layer_3d_desc<T_I1, T_I2, T_I3, T_C1, T_C2, T_C3> {
     /*! The layer type */
-    using layer_t = avgp_layer_3d<avgp_layer_3d_desc<I1, I2, I3, C1, C2, C3>>;
+    using layer_t = avgp_layer_3d<avgp_layer_3d_desc<T_I1, T_I2, T_I3, T_C1, T_C2, T_C3>>;
 };
 
 } //end of dll namespace
