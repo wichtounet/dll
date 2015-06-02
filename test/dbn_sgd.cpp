9bca247e08da463e01322ccb606a1df7f2e3f834
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-06-02 15:06:29 +0200

    Test for SGD in memory mode

--- test/dbn_sgd.cpp
+++ test/dbn_sgd.cpp
@@ -180,3 +180,35 @@ TEST_CASE( "dbn/sgd/6", "[dbn][mnist][sgd]" ) {
 
     REQUIRE(error < 5e-2);
 }
+
+TEST_CASE( "dbn/sgd/7", "[dbn][mnist][sgd][memory]" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::rbm_desc<28 * 28, 100, dll::momentum, dll::batch_size<25>, dll::init_weights>::rbm_t,
+            dll::rbm_desc<100, 200, dll::momentum, dll::batch_size<25>>::rbm_t,
+            dll::rbm_desc<200, 10, dll::momentum, dll::batch_size<25>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t>,
+        dll::trainer<dll::sgd_trainer>,
+        dll::memory
+        >::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(500);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->learning_rate = 0.1;
+
+    dbn->pretrain(dataset.training_images, 20);
+    auto error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 100, 10);
+
+    REQUIRE(error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+
+    std::cout << "test_error:" << test_error << std::endl;
+
+    REQUIRE(test_error < 0.2);
+}
