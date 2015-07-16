8553490647d1d30b822e367866fbc39ae967063e
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-07-16 14:19:26 +0200

    New test

--- test/dense.cpp
+++ test/dense.cpp
@@ -119,3 +119,35 @@ TEST_CASE( "dense/sgd/3", "[dense][dbn][mnist][sgd]" ) {
     std::cout << "test_error:" << test_error << std::endl;
     REQUIRE(test_error < 0.2);
 }
+
+TEST_CASE( "dense/sgd/4", "[dense][dbn][mnist][sgd]" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::dense_desc<28 * 28, 100>::layer_t,
+            dll::dense_desc<100, 10>::layer_t
+        >
+        , dll::momentum
+        , dll::weight_decay<>
+        , dll::trainer<dll::dense_sgd_trainer>
+    >::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(1000);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist_scale(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->initial_momentum = 0.9;
+    dbn->final_momentum = 0.9;
+    dbn->learning_rate = 0.01;
+
+    auto ft_error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 100, 10);
+    std::cout << "ft_error:" << ft_error << std::endl;
+
+    CHECK(ft_error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
