d76887383901f306ab80dbef8b1df701a9f0d9b5
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-07-16 12:04:58 +0200

    New test

--- test/dense.cpp
+++ test/dense.cpp
@@ -46,6 +46,33 @@ TEST_CASE( "dense/sgd/1", "[dense][dbn][mnist][sgd]" ) {
 TEST_CASE( "dense/sgd/2", "[dense][dbn][mnist][sgd]" ) {
     typedef dll::dbn_desc<
         dll::dbn_layers<
+            dll::dense_desc<28 * 28, 100, dll::activation<dll::function::TANH>>::layer_t,
+            dll::dense_desc<100, 10, dll::activation<dll::function::TANH>>::layer_t
+        >,
+        dll::trainer<dll::dense_sgd_trainer>
+    >::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, double>(1000);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->learning_rate = 0.05;
+
+    auto ft_error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 100, 10);
+    std::cout << "ft_error:" << ft_error << std::endl;
+
+    CHECK(ft_error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
+
+TEST_CASE( "dense/sgd/3", "[dense][dbn][mnist][sgd]" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
             dll::dense_desc<28 * 28, 100>::layer_t,
             dll::dense_desc<100, 10>::layer_t
         >
