517f1356d0f17226f904cd61201201a2ebd648f7
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-08-31 14:22:24 +0200

    Prepare header for generated file

---
+++ include/dll/processor/processor.hpp
@@ -0,0 +1,23 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*!
+ * \file processor.hpp
+ * \brief This file is made to be included by the dllp generated file only.
+ */
+
+#include "dll/dbn.hpp"
+
+namespace dll {
+
+namespace processor {
+
+//TODO
+
+} //end of namespace processor
+
+} //end of namespace dll
