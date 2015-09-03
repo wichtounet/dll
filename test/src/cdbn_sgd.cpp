ec7e50b00286d1e7a170384f4f6d5008e198810e
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-09-03 13:12:30 +0200

    Add new tests

---
+++ test/src/cdbn_sgd.cpp
@@ -0,0 +1,108 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include "catch.hpp"
+
+#include "dll/conv_rbm.hpp"
+#include "dll/dbn.hpp"
+#include "dll/dense_stochastic_gradient_descent.hpp"
+
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
+
+TEST_CASE( "cdbn/sgd/1", "[dbn][mnist][sgd]" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_rbm_desc_square<1, 28, 10, 12, dll::momentum, dll::batch_size<10>, dll::weight_type<float>>::rbm_t,
+            dll::rbm_desc<12 * 12 * 10, 10, dll::momentum, dll::batch_size<10>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t
+            >
+        , dll::trainer<dll::dense_sgd_trainer>
+        , dll::batch_size<10>
+        >::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    dbn->learning_rate = 0.1;
+
+    auto ft_error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 50);
+    std::cout << "ft_error:" << ft_error << std::endl;
+    CHECK(ft_error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
+
+TEST_CASE( "cdbn/sgd/2", "[dbn][mnist][sgd]" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_rbm_desc_square<1, 28, 10, 12, dll::momentum, dll::batch_size<10>, dll::weight_type<float>>::rbm_t,
+            dll::conv_rbm_desc_square<10, 12, 6, 6, dll::momentum, dll::batch_size<10>, dll::weight_type<float>>::rbm_t,
+            dll::rbm_desc<6 * 6 * 6, 10, dll::momentum, dll::batch_size<10>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t
+            >
+        , dll::trainer<dll::dense_sgd_trainer>
+        , dll::batch_size<10>
+        >::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    dbn->learning_rate = 0.1;
+
+    auto ft_error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 50);
+    std::cout << "ft_error:" << ft_error << std::endl;
+    CHECK(ft_error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
+
+TEST_CASE( "cdbn/sgd/3", "[dbn][mnist][sgd]" ) {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_rbm_desc_square<1, 28, 10, 20, dll::momentum, dll::batch_size<10>, dll::weight_type<float>>::rbm_t,
+            dll::conv_rbm_desc_square<10, 20, 10, 14, dll::momentum, dll::batch_size<10>, dll::weight_type<float>>::rbm_t,
+            dll::rbm_desc<10 * 14 * 14, 700, dll::momentum, dll::batch_size<10>>::rbm_t,
+            dll::rbm_desc<700, 10, dll::momentum, dll::batch_size<10>, dll::hidden<dll::unit_type::SOFTMAX>>::rbm_t
+            >
+        , dll::trainer<dll::dense_sgd_trainer>
+        , dll::batch_size<10>
+        >::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    dbn->learning_rate = 0.1;
+
+    auto ft_error = dbn->fine_tune(dataset.training_images, dataset.training_labels, 50);
+    std::cout << "ft_error:" << ft_error << std::endl;
+    CHECK(ft_error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
