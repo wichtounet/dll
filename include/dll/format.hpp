31437aeabd5d13a23b0b1d7b8ac2ce2bc3882b2f
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-11-09 13:18:57 +0100

    Enumeration for output formats

---
+++ include/dll/format.hpp
@@ -0,0 +1,27 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*!
+ * \file format.hpp
+ * \brief Enumeration for the supported input/output formats of the library.
+ */
+
+#ifndef DLL_FORMAT_HPP
+#define DLL_FORMAT_HPP
+
+namespace dll {
+
+/*!
+ * \brief An activation function
+ */
+enum class format {
+    DLL ///< Default simple format of the DLL library
+};
+
+} //end of dll namespace
+
+#endif
