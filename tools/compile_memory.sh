ed3d675417e2c715185cc374adc3a14eeae06c16
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-14 11:10:49 +0100

    Update script

--- tools/compile_memory.sh
+++ tools/compile_memory.sh
@@ -4,7 +4,9 @@ make clean
 
 for f in test/src/*.cpp
 do
-    rss=`/usr/bin/time -v make debug/$f.o 2>&1 | grep "Maximum resident set size" | rev | cut -d" " -f1 | rev`
+    results=$(/usr/bin/time -v make debug/$f.o 2>&1)
+    rss=$(echo "$results" | grep "Maximum resident set size" | rev | cut -d" " -f1 | rev)
+    elapsed=$(echo "$results" | grep "Elapsed" | rev | cut -d" " -f1 | rev)
     memory=$(echo "scale=2; $rss/1024" | bc -l)
-    echo "$f => $rss => ${memory}MB"
+    echo "$f => $elapsed => ${memory}MB (rss:$rss)"
 done
