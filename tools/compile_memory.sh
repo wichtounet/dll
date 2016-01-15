4becb1f6aa8d18f4ebf50c01eba5624e3fbc2634
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-01-15 09:26:12 +0100

    Better debugging tool

--- tools/compile_memory.sh
+++ tools/compile_memory.sh
@@ -2,6 +2,11 @@
 
 make clean
 
+max_memory="0.0"
+total="0.0"
+
+start=$(date "+%s.%N")
+
 for f in test/src/*.cpp
 do
     results=$(/usr/bin/time -v make debug/$f.o 2>&1)
@@ -9,4 +14,16 @@ do
     elapsed=$(echo "$results" | grep "Elapsed" | rev | cut -d" " -f1 | rev)
     memory=$(echo "scale=2; $rss/1024" | bc -l)
     echo "$f => $elapsed => ${memory}MB (rss:$rss)"
+
+    if [ $(echo "$max_memory < $memory" | bc) -eq 1 ]
+    then
+        max_memory=$memory
+    fi
 done
+
+end=$(date "+%s.%N")
+
+runtime=$(echo "scale=3; ($end - $start) / 1.0" | bc -l)
+
+echo "Max memory: $max_memory"
+echo "Total time: $runtime"
