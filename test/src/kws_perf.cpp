d2eb52c0c35fb0d8b8e53ea2d8f93dbb30f305bc
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-18 10:21:27 +0100

    New perf test

--- test/src/kws_perf.cpp
+++ test/src/kws_perf.cpp
@@ -29,8 +29,8 @@ TEST_CASE("perf/kws_square", "[perf][crbm][mp][cdbn]") {
                 dll::conv_rbm_desc<
                     8, 10, 10, 8, 8, 8, dll::weight_type<float>, dll::batch_size<64>, dll::momentum,
                     dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>, dll::shuffle_cond<true>, dll::dbn_only>::layer_t,
-                dll::mp_layer_3d_desc<8, 8, 8, 1, 2, 2, dll::weight_type<float>>::layer_t>
-            >::dbn_t;
+                dll::mp_layer_3d_desc<8, 8, 8, 1, 2, 2, dll::weight_type<float>>::layer_t
+            >>::dbn_t;
 
     auto cdbn = std::make_unique<cdbn_t>();
 
@@ -56,14 +56,48 @@ TEST_CASE("perf/kws", "[perf][crbm][mp][cdbn]") {
                 dll::conv_rbm_desc<
                     8, 16, 6, 8, 14, 4, dll::weight_type<float>, dll::batch_size<64>, dll::momentum,
                     dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>, dll::shuffle_cond<true>, dll::dbn_only>::layer_t,
-                dll::mp_layer_3d_desc<8, 14, 4, 1, 2, 2, dll::weight_type<float>>::layer_t>
-            >::dbn_t;
+                dll::mp_layer_3d_desc<8, 14, 4, 1, 2, 2, dll::weight_type<float>>::layer_t
+            >>::dbn_t;
 
     auto cdbn = std::make_unique<cdbn_t>();
 
     cdbn->display();
 
     auto dataset = mnist::read_dataset<std::vector, std::vector, float>(8192);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    std::vector<etl::fast_dyn_matrix<float, 1, 40, 20>> augmented(dataset.training_images.size());
+
+    for(std::size_t i = 0; i < dataset.training_images.size(); ++i){
+        auto& source_image = dataset.training_images[i];
+        auto& target_image = augmented[i];
+
+        for(std::size_t j = 0; j < source_image.size(); ++j){
+            target_image[j] = source_image[j];
+        }
+    }
+
+    cdbn->pretrain(augmented, 5);
+}
+
+TEST_CASE("perf/kws_sub", "[perf][crbm][mp][cdbn]") {
+    using cdbn_t =
+        dll::dbn_desc<
+            dll::dbn_layers<
+                dll::conv_rbm_desc<
+                    1, 40, 20, 8, 32, 12, dll::weight_type<float>, dll::batch_size<64>, dll::momentum,
+                    dll::weight_decay<dll::decay_type::L2>, dll::sparsity<dll::sparsity_method::LEE>, dll::shuffle_cond<true>, dll::dbn_only>::layer_t,
+                dll::mp_layer_3d_desc<8, 32, 12, 1, 2, 2, dll::weight_type<float>>::layer_t
+            >>::dbn_t;
+
+    auto cdbn = std::make_unique<cdbn_t>();
+
+    cdbn->display();
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, float>(16384);
 
     REQUIRE(!dataset.training_images.empty());
 
