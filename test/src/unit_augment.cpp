77c9a865f97134ea0b927d5b5d7ae311564be94e
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-04-01 11:50:58 +0200

    Test for elastic distortion

--- test/src/unit_augment.cpp
+++ test/src/unit_augment.cpp
@@ -113,6 +113,46 @@ TEST_CASE("unit/augment/mnist/5", "[cdbn][augment][unit]") {
     dbn->pretrain(dataset.training_images, 20);
 }
 
+TEST_CASE("unit/augment/mnist/6", "[cdbn][augment][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+              dll::augment_layer_desc<dll::elastic<3>>::layer_t
+            , dll::patches_layer_desc<14, 14, 14, 14>::layer_t
+            , dll::conv_rbm_desc_square<1, 14, 10, 8, dll::momentum, dll::batch_size<10>>::layer_t
+        >>::dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+}
+
+TEST_CASE("unit/augment/mnist/7", "[cdbn][augment][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+              dll::augment_layer_desc<dll::elastic<3>>::layer_t
+            , dll::patches_layer_desc<14, 14, 14, 14>::layer_t
+            , dll::conv_rbm_desc_square<1, 14, 10, 8, dll::momentum, dll::batch_size<10>>::layer_t
+        >, dll::batch_mode>::dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+}
+
 //TODO Make this work (harder than it seems with current architecture)
 //TEST_CASE("unit/augment/mnist/100", "[cdbn][augment][unit]") {
     //using dbn_t =
