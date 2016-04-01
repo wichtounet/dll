59c1f7a98dd5c8ca07e92c06f3584de949485b36
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-04-01 09:19:43 +0200

    Add test cases with batch mode

--- test/src/unit_augment.cpp
+++ test/src/unit_augment.cpp
@@ -34,8 +34,27 @@ TEST_CASE("unit/augment/mnist/1", "[cdbn][augment][unit]") {
     dbn->pretrain(dataset.training_images, 20);
 }
 
-// This is here as a a test for multiplex layers
 TEST_CASE("unit/augment/mnist/2", "[cdbn][augment][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+            dll::augment_layer_desc<dll::copy<2>, dll::copy<3>>::layer_t,
+            dll::conv_rbm_desc_square<1, 28, 20, 8, dll::momentum, dll::batch_size<10>>::layer_t
+        >, dll::batch_mode>::dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<double, 1, 28, 28>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+}
+
+// This is here as a a test for multiplex layers (compilation)
+TEST_CASE("unit/augment/mnist/3", "[cdbn][augment][unit]") {
     using dbn_t =
         dll::dbn_desc<dll::dbn_layers<
               dll::patches_layer_desc<14, 14, 14, 14>::layer_t
@@ -54,13 +73,33 @@ TEST_CASE("unit/augment/mnist/2", "[cdbn][augment][unit]") {
     dbn->pretrain(dataset.training_images, 2);
 }
 
-TEST_CASE("unit/augment/mnist/3", "[cdbn][augment][unit]") {
+TEST_CASE("unit/augment/mnist/4", "[cdbn][augment][unit]") {
     using dbn_t =
         dll::dbn_desc<dll::dbn_layers<
               dll::augment_layer_desc<dll::copy<2>, dll::copy<3>>::layer_t
             , dll::patches_layer_desc<14, 14, 14, 14>::layer_t
             , dll::conv_rbm_desc_square<1, 14, 10, 8, dll::momentum, dll::batch_size<10>>::layer_t
         >>::dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+}
+
+TEST_CASE("unit/augment/mnist/5", "[cdbn][augment][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+              dll::augment_layer_desc<dll::copy<2>, dll::copy<3>>::layer_t
+            , dll::patches_layer_desc<14, 14, 14, 14>::layer_t
+            , dll::conv_rbm_desc_square<1, 14, 10, 8, dll::momentum, dll::batch_size<10>>::layer_t
+        >, dll::batch_mode>::dbn_t;
 
     auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(100);
     REQUIRE(!dataset.training_images.empty());
