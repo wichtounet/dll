27c829f61cee09c198837348fa3f04316bd38da5
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-04-18 10:59:39 +0200

    Rework the data

--- workbench/src/perf_paper.cpp
+++ workbench/src/perf_paper.cpp
@@ -39,17 +39,34 @@ struct perf_timer {
 
 int main(int argc, char* argv []) {
     auto dataset = mnist::read_dataset_direct<std::vector, etl::dyn_vector<float>>();
+    dataset.training_images.resize(1000);
 
     std::string number;
     if(argc > 1){
         number = argv[1];
     }
 
+    auto n = dataset.training_images.size();
+
     mnist::binarize_dataset(dataset);
 
-    dataset.training_images.resize(1000);
+    decltype(auto) data_1 = dataset.training_images;
+    std::vector<etl::dyn_vector<float>> data_2(n, etl::dyn_vector<float>(500));
+    std::vector<etl::dyn_vector<float>> data_3(n, etl::dyn_vector<float>(500));
+    std::vector<etl::dyn_vector<float>> data_4(n, etl::dyn_vector<float>(2000));
 
-    std::cout << dataset.training_images.size() << " images used for training" << std::endl;
+    for(std::size_t i = 0; i < n; ++i){
+        data_2[i] = etl::normal_generator();
+        data_3[i] = etl::normal_generator();
+        data_4[i] = etl::normal_generator();
+    }
+
+    cpp_assert(data_1[0].size() == 784, "Invalid input size");
+    cpp_assert(data_2[0].size() == 500, "Invalid input size");
+    cpp_assert(data_3[0].size() == 500, "Invalid input size");
+    cpp_assert(data_4[0].size() == 2000, "Invalid input size");
+
+    std::cout << n << " images used for training" << std::endl;
 
     if(number.empty() || number == "1"){
         dll::rbm_desc<784, 500, dll::parallel_mode, dll::serial, dll::weight_type<float>>::layer_t rbm_1;
@@ -59,22 +76,22 @@ int main(int argc, char* argv []) {
 
         {
             perf_timer timer("rbm_784_500_normal", EPOCHS);
-            rbm_1.train<false>(dataset.training_images, EPOCHS);
+            rbm_1.train<false>(data_1, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_500_500_normal", EPOCHS);
-            rbm_2.train<false>(dataset.training_images, EPOCHS);
+            rbm_2.train<false>(data_2, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_500_2000_normal", EPOCHS);
-            rbm_3.train<false>(dataset.training_images, EPOCHS);
+            rbm_3.train<false>(data_3, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_2000_10_normal", EPOCHS);
-            rbm_4.train<false>(dataset.training_images, EPOCHS);
+            rbm_4.train<false>(data_4, EPOCHS);
         }
     }
 
@@ -86,22 +103,22 @@ int main(int argc, char* argv []) {
 
         {
             perf_timer timer("rbm_784_500_par_64", EPOCHS);
-            rbm_1.train<false>(dataset.training_images, EPOCHS);
+            rbm_1.train<false>(data_1, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_500_500_par_64", EPOCHS);
-            rbm_2.train<false>(dataset.training_images, EPOCHS);
+            rbm_2.train<false>(data_2, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_500_2000_par_64", EPOCHS);
-            rbm_3.train<false>(dataset.training_images, EPOCHS);
+            rbm_3.train<false>(data_3, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_2000_10_par_64", EPOCHS);
-            rbm_4.train<false>(dataset.training_images, EPOCHS);
+            rbm_4.train<false>(data_4, EPOCHS);
         }
     }
 
@@ -113,22 +130,22 @@ int main(int argc, char* argv []) {
 
         {
             perf_timer timer("rbm_784_500_batch_64", EPOCHS);
-            rbm_1.train<false>(dataset.training_images, EPOCHS);
+            rbm_1.train<false>(data_1, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_500_500_batch_64", EPOCHS);
-            rbm_2.train<false>(dataset.training_images, EPOCHS);
+            rbm_2.train<false>(data_2, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_500_2000_batch_64", EPOCHS);
-            rbm_3.train<false>(dataset.training_images, EPOCHS);
+            rbm_3.train<false>(data_3, EPOCHS);
         }
 
         {
             perf_timer timer("rbm_2000_10_batch_64", EPOCHS);
-            rbm_4.train<false>(dataset.training_images, EPOCHS);
+            rbm_4.train<false>(data_4, EPOCHS);
         }
     }
 
