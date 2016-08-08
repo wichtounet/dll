5a0fb0ec7875ebdd30295fcdc2d92436d94a467e
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-08 08:39:17 +0200

    Add new test case

--- test/src/dyn_dbn_cg.cpp
+++ test/src/dyn_dbn_cg.cpp
@@ -47,3 +47,42 @@ TEST_CASE("dyn_dbn/cg/mnist/1", "dbn::simple") {
     std::cout << "test_error:" << test_error << std::endl;
     REQUIRE(test_error < 0.2);
 }
+
+TEST_CASE("dyn_dbn/cg/mnist/2", "dbn::memory") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::dyn_rbm_desc<dll::momentum, dll::init_weights>::layer_t,
+            dll::dyn_rbm_desc<dll::momentum>::layer_t,
+            dll::dyn_rbm_desc<dll::momentum, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>,
+        dll::batch_mode, dll::batch_size<50>, dll::big_batch_size<3>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::dyn_matrix<float, 1>>(1078);
+
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->template layer_get<0>().init_rbm(28 * 28, 100);
+    dbn->template layer_get<1>().init_rbm(100, 200);
+    dbn->template layer_get<2>().init_rbm(200, 10);
+
+    dbn->pretrain(dataset.training_images, 20);
+    auto error = dbn->fine_tune(
+        dataset.training_images.begin(), dataset.training_images.end(),
+        dataset.training_labels.begin(), dataset.training_labels.end(),
+        10);
+
+    REQUIRE(error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+
+    std::cout << "test_error:" << test_error << std::endl;
+
+    REQUIRE(test_error < 0.2);
+
+    //Mostly here to ensure compilation
+    auto out = dbn->prepare_one_output<etl::dyn_matrix<float, 1>>();
+    REQUIRE(out.size() > 0);
+}
