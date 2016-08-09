72e96742f8149362a0ddd74f81f58ecc9caf8faf
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-09 11:10:48 +0200

    New bench for float/double
    Turns out the performance drop was caused by denormals

--- Makefile
+++ Makefile
@@ -151,6 +151,7 @@ $(eval $(call add_executable_set,dll_view,dll_view_rbm, dll_view_crbm, dll_view_
 $(eval $(call add_executable,dll_perf_paper,workbench/src/perf_paper.cpp))
 $(eval $(call add_executable,dll_perf_paper_conv,workbench/src/perf_paper_conv.cpp))
 $(eval $(call add_executable,dll_perf_conv,workbench/src/perf_conv.cpp))
+$(eval $(call add_executable,dll_conv_types,workbench/src/conv_types.cpp))
 $(eval $(call add_executable,dll_compile_rbm_one,workbench/src/compile_rbm_one.cpp))
 $(eval $(call add_executable,dll_compile_dyn_rbm_one,workbench/src/compile_dyn_rbm_one.cpp))
 $(eval $(call add_executable,dll_compile_rbm,workbench/src/compile_rbm.cpp))
@@ -162,6 +163,7 @@ $(eval $(call add_executable,dll_dyn_perf,workbench/src/dyn_perf.cpp))
 $(eval $(call add_executable_set,dll_perf_paper,dll_perf_paper))
 $(eval $(call add_executable_set,dll_perf_paper_conv,dll_perf_paper_conv))
 $(eval $(call add_executable_set,dll_perf_conv,dll_perf_conv))
+$(eval $(call add_executable_set,dll_conv_types,dll_conv_types))
 
 release: release_dllp release_dll_test release_dll_view
 release_debug: release_debug_dllp release_debug_dll_test release_debug_dll_view
---
+++ workbench/src/conv_types.cpp
@@ -0,0 +1,75 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <iostream>
+#include <chrono>
+
+#include <fenv.h>
+
+#include "dll/conv_rbm.hpp"
+
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
+
+namespace {
+
+constexpr const std::size_t EPOCHS = 10;
+
+using clock      = std::chrono::steady_clock;
+using time_point = std::chrono::time_point<clock>;
+using resolution = std::chrono::milliseconds;
+
+#define MEASURE(rbm, name, data)                                                                                                    \
+    {                                                                                                                               \
+        std::size_t d_min  = std::numeric_limits<std::size_t>::max();                                                               \
+        std::size_t d_max  = 0;                                                                                                     \
+        std::size_t d_mean = 0;                                                                                                     \
+        for (std::size_t i = 0; i < EPOCHS; ++i) {                                                                                  \
+            time_point start = clock::now();                                                                                        \
+            rbm.train<false>(data, 1);                                                                                              \
+            time_point end = clock::now();                                                                                          \
+            std::size_t d  = std::chrono::duration_cast<resolution>(end - start).count();                                           \
+            d_min          = std::min(d_min, d);                                                                                    \
+            d_max          = std::max(d_max, d);                                                                                    \
+            d_mean         = d_mean + d;                                                                                            \
+        }                                                                                                                           \
+        std::cout << name << ": min:" << d_min << "ms max:" << d_max << "ms mean:" << d_mean / double(EPOCHS) << "ms" << std::endl; \
+    }
+
+} //end of anonymous namespace
+
+int main(int argc, char* argv []) {
+    // With this flag, float version is much faster
+    // _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+
+    auto dataset = mnist::read_dataset<std::vector, std::vector, float>(2500);
+
+    std::string sub;
+    if(argc > 1){
+        sub = argv[1];
+    }
+
+    auto n = dataset.training_images.size();
+
+    mnist::normalize_dataset(dataset);
+
+    std::cout << n << " images used for training" << std::endl;
+    std::cout << etl::threads << " maximum threads" << std::endl;
+
+    if(sub.empty() || sub == "batch"){
+        dll::conv_rbm_desc_square<1, 28, 20, 12, dll::visible<dll::unit_type::GAUSSIAN>, dll::weight_decay<dll::decay_type::L2>, dll::momentum, dll::shuffle, dll::batch_size<25>, dll::weight_type<float>>::layer_t crbm_float;
+        dll::conv_rbm_desc_square<1, 28, 20, 12, dll::visible<dll::unit_type::GAUSSIAN>, dll::weight_decay<dll::decay_type::L2>, dll::momentum, dll::shuffle, dll::batch_size<25>, dll::weight_type<double>>::layer_t crbm_double;
+        MEASURE(crbm_float, "batch_float", dataset.training_images);
+        MEASURE(crbm_double, "batch_double", dataset.training_images);
+    }
+
+    if(!sub.empty()){
+        dll::dump_timers();
+    }
+
+    return 0;
+}
