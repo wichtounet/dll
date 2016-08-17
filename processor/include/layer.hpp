1494095ed50c3507261e827bc472f49aa05bb1f6
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-17 10:53:08 +0200

    Some doc

--- processor/include/layer.hpp
+++ processor/include/layer.hpp
@@ -19,20 +19,45 @@ struct layer;
 
 using layers_t = std::vector<std::unique_ptr<dllp::layer>>;
 
+/*!
+ * \brief A layer (in the processor configuration)
+ */
 struct layer {
+    /*!
+     * \brief Print the layer code to the given stream
+     * \param out The stream to print to
+     */
     virtual void print(std::ostream& out) const = 0;
+
+    /*!
+     * \brief Returns the number of hidden unit
+     */
     virtual std::size_t hidden_get() const = 0;
 
+    /*!
+     * \brief Indicates if the layer is convolutional
+     */
     virtual bool is_conv() const {
         return false;
     }
 
+    /*!
+     * \brief Returns the first dimension of the output matrix
+     */
     virtual std::size_t hidden_get_1() const {
         return 0;
     }
+
+    /*!
+     * \brief Returns the second dimension of the output matrix
+     */
     virtual std::size_t hidden_get_2() const {
         return 0;
     }
+
+    /*!
+     * \brief Returns the third dimension of the output matrix
+     */
     virtual std::size_t hidden_get_3() const {
         return 0;
     }
@@ -49,16 +74,16 @@ enum class parse_result {
 };
 
 struct base_rbm_layer : layer {
-    std::string visible_unit;
-    std::string hidden_unit;
+    std::string visible_unit; ///< The visible unit type
+    std::string hidden_unit;  ///< The hidden unit type
 
-    double learning_rate   = dll::processor::stupid_default;
-    double momentum        = dll::processor::stupid_default;
-    std::size_t batch_size = 0;
+    double learning_rate   = dll::processor::stupid_default; ///< The learning rate
+    double momentum        = dll::processor::stupid_default; ///< The momentum
+    std::size_t batch_size = 0;                              ///< The batch size
 
-    std::string decay     = "none";
-    double l1_weight_cost = dll::processor::stupid_default;
-    double l2_weight_cost = dll::processor::stupid_default;
+    std::string decay     = "none";                         ///< The type of decay
+    double l1_weight_cost = dll::processor::stupid_default; ///< The L1 decay rate
+    double l2_weight_cost = dll::processor::stupid_default; ///< The L2 decay rate
 
     std::string sparsity   = "none";
     double sparsity_target = dll::processor::stupid_default;
@@ -77,8 +102,8 @@ struct base_rbm_layer : layer {
 };
 
 struct rbm_layer : base_rbm_layer {
-    std::size_t visible = 0;
-    std::size_t hidden  = 0;
+    std::size_t visible = 0; ///< The number of visible units
+    std::size_t hidden  = 0; ///< The number of hidden units
 
     void print(std::ostream& out) const override;
     bool parse(const layers_t& layers, const std::vector<std::string>& lines, std::size_t& i) override;
@@ -87,12 +112,12 @@ struct rbm_layer : base_rbm_layer {
 };
 
 struct conv_rbm_layer : base_rbm_layer {
-    std::size_t c  = 0;
-    std::size_t v1 = 0;
-    std::size_t v2 = 0;
-    std::size_t k  = 0;
-    std::size_t w1 = 0;
-    std::size_t w2 = 0;
+    std::size_t c  = 0; ///< The number of channels
+    std::size_t v1 = 0; ///< The first dimension of the output
+    std::size_t v2 = 0; ///< The second dimension of the output
+    std::size_t k  = 0; ///< The number of filters
+    std::size_t w1 = 0; ///< The first dimension of the filters
+    std::size_t w2 = 0; ///< The second dimension of the filters
 
     void print(std::ostream& out) const override;
     bool parse(const layers_t& layers, const std::vector<std::string>& lines, std::size_t& i) override;
@@ -105,12 +130,12 @@ struct conv_rbm_layer : base_rbm_layer {
 };
 
 struct conv_rbm_mp_layer : base_rbm_layer {
-    std::size_t c  = 0;
-    std::size_t v1 = 0;
-    std::size_t v2 = 0;
-    std::size_t k  = 0;
-    std::size_t w1 = 0;
-    std::size_t w2 = 0;
+    std::size_t c  = 0; ///< The number of channels
+    std::size_t v1 = 0; ///< The first dimension of the output
+    std::size_t v2 = 0; ///< The second dimension of the output
+    std::size_t k  = 0; ///< The number of filters
+    std::size_t w1 = 0; ///< The first dimension of the filters
+    std::size_t w2 = 0; ///< The second dimension of the filters
     std::size_t p  = 0;
 
     void print(std::ostream& out) const override;
@@ -124,8 +149,8 @@ struct conv_rbm_mp_layer : base_rbm_layer {
 };
 
 struct dense_layer : layer {
-    std::size_t visible = 0;
-    std::size_t hidden  = 0;
+    std::size_t visible = 0; ///< The number of visible units
+    std::size_t hidden  = 0; ///< The number of hidden units
 
     std::string activation;
 
@@ -136,12 +161,12 @@ struct dense_layer : layer {
 };
 
 struct conv_layer : layer {
-    std::size_t c  = 0;
-    std::size_t v1 = 0;
-    std::size_t v2 = 0;
-    std::size_t k  = 0;
-    std::size_t w1 = 0;
-    std::size_t w2 = 0;
+    std::size_t c  = 0; ///< The number of channels
+    std::size_t v1 = 0; ///< The first dimension of the output
+    std::size_t v2 = 0; ///< The second dimension of the output
+    std::size_t k  = 0; ///< The number of filters
+    std::size_t w1 = 0; ///< The first dimension of the filters
+    std::size_t w2 = 0; ///< The second dimension of the filters
 
     std::string activation;
 
