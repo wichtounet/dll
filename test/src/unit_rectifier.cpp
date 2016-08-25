99455f75626c21a76f7eb501e5706cbacece5004
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-25 11:22:56 +0200

    Test rectifier layer in dyn networks

--- test/src/unit_rectifier.cpp
+++ test/src/unit_rectifier.cpp
@@ -9,6 +9,7 @@
 
 #define DLL_SVM_SUPPORT
 
+#include "dll/dyn_conv_rbm.hpp"
 #include "dll/conv_rbm.hpp"
 #include "dll/rectifier_layer.hpp"
 #include "dll/dbn.hpp"
@@ -58,6 +59,37 @@ TEST_CASE("unit/cdbn/rectifier/mnist/2", "[cdbn][rectifier][svm][unit]") {
     mnist::binarize_dataset(dataset);
 
     auto dbn = std::make_unique<dbn_t>();
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.1);
+}
+
+TEST_CASE("unit/cdbn/rectifier/mnist/3", "[cdbn][rectifier][svm][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+              dll::dyn_conv_rbm_desc<dll::parallel_mode, dll::momentum>::layer_t
+            , dll::rectifier_layer_desc<>::layer_t
+            , dll::dyn_conv_rbm_desc<dll::parallel_mode, dll::momentum>::layer_t
+            , dll::rectifier_layer_desc<>::layer_t
+        >>::dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->template layer_get<0>().init_layer(1, 28, 28, 20, 12, 12);
+    dbn->template layer_get<2>().init_layer(20, 12, 12, 20, 10, 10);
 
     dbn->display();
 
