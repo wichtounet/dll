f2c34ca9d99bf58e1d52c421e376cc31bcfafd90
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-25 15:38:01 +0200

    Test mixing LCN and dyn

--- test/src/unit_lcn.cpp
+++ test/src/unit_lcn.cpp
@@ -9,6 +9,7 @@
 
 #define DLL_SVM_SUPPORT
 
+#include "dll/dyn_conv_rbm.hpp"
 #include "dll/conv_rbm.hpp"
 #include "dll/dbn.hpp"
 #include "dll/rectifier_layer.hpp"
@@ -196,4 +197,34 @@ TEST_CASE("unit/cdbn/lcn/mnist/6", "[cdbn][lcn][unit]") {
     dbn->display();
 
     dbn->pretrain(dataset.training_images, 20);
+}
+
+TEST_CASE("unit/cdbn/lcn/mnist/7", "[cdbn][lcn][svm][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+              dll::dyn_conv_rbm_desc<dll::parallel_mode, dll::momentum>::layer_t
+            , dll::dyn_conv_rbm_desc<dll::parallel_mode, dll::momentum>::layer_t
+            , dll::lcn_layer_desc<9>::layer_t
+        >>::dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->template init_layer<0>(1, 28, 28, 20, 12, 12);
+    dbn->template init_layer<1>(20, 12, 12, 20, 10, 10);
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.1);
 }
