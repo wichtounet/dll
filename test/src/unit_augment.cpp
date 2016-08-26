6f6ea5704a58fee84fd297b8c8e33d21feddf7f2
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-26 08:26:53 +0200

    Test for the new layer

--- test/src/unit_augment.cpp
+++ test/src/unit_augment.cpp
@@ -11,6 +11,7 @@
 #include "dll/conv_rbm.hpp"
 #include "dll/augment_layer.hpp"
 #include "dll/patches_layer.hpp"
+#include "dll/dyn_patches_layer.hpp"
 #include "dll/dbn.hpp"
 
 #include "mnist/mnist_reader.hpp"
@@ -205,6 +206,30 @@ TEST_CASE("unit/augment/mnist/9", "[cdbn][augment][unit]") {
     dbn->display();
 
     dbn->pretrain(dataset.training_images, 20);
+}
+
+TEST_CASE("unit/augment/mnist/10", "[cdbn][augment][unit]") {
+    using dbn_t =
+        dll::dbn_desc<dll::dbn_layers<
+              dll::dyn_patches_layer_desc<>::layer_t
+            , dll::dyn_conv_rbm_desc<dll::momentum>::layer_t
+        >>::dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<float, 3>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->template init_layer<0>(14, 14, 14, 14);
+    dbn->template init_layer<1>(1, 14, 14, 10, 8, 8);
+
+    dbn->display();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    REQUIRE(dbn->activation_probabilities(dataset.training_images[0]).size() > 0);
 }
 
 //TODO Make this work (harder than it seems with current architecture)
