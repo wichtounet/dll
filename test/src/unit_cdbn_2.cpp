6fcac9b1071c750f31d0ebed332223e0c1be08fe
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-26 13:30:05 +0200

    Split some unit tests
    Memory consumption was going too crazy :(

---
+++ test/src/unit_cdbn_2.cpp
@@ -0,0 +1,174 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include "catch.hpp"
+
+#define DLL_SVM_SUPPORT
+
+#include "dll/conv_rbm.hpp"
+#include "dll/dyn_conv_rbm.hpp"
+#include "dll/conv_rbm_mp.hpp"
+#include "dll/mp_layer.hpp"
+#include "dll/avgp_layer.hpp"
+#include "dll/patches_layer.hpp"
+#include "dll/patches_layer_pad.hpp"
+#include "dll/dyn_patches_layer_pad.hpp"
+#include "dll/dbn.hpp"
+
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
+
+TEST_CASE("unit/cdbn/mnist/7", "[cdbn][ap][svm][unit]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_rbm_desc<1, 28, 28, 20, 20, 21, dll::momentum, dll::batch_size<25>>::layer_t,
+            dll::avgp_layer_3d_desc<20, 20, 21, 2, 2, 3>::layer_t,
+            dll::conv_rbm_desc<10, 10, 7, 20, 8, 5, dll::momentum, dll::batch_size<25>>::layer_t,
+            dll::avgp_layer_3d_desc<20, 8, 5, 2, 1, 1>::layer_t>>::dbn_t dbn_t;
+
+    REQUIRE(dbn_t::output_size() == 400);
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<double, 1, 28, 28>>(200);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto output = dbn->activation_probabilities(dataset.training_images.front());
+    REQUIRE(output.size() == 400);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.5);
+}
+
+TEST_CASE("unit/cdbn/mnist/8", "[cdbn][ap][svm][unit]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_rbm_desc<1, 28, 28, 20, 14, 12, dll::momentum, dll::batch_size<25>>::layer_t,
+            dll::conv_rbm_desc<20, 14, 12, 20, 8, 10, dll::momentum, dll::batch_size<25>>::layer_t, dll::mp_layer_3d_desc<20, 8, 10, 1, 1, 1>::layer_t, dll::avgp_layer_3d_desc<20, 8, 10, 1, 1, 1>::layer_t>>::dbn_t dbn_t;
+
+    REQUIRE(dbn_t::output_size() == 1600);
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<double, 1, 28, 28>>(100);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 20);
+
+    auto output = dbn->activation_probabilities(dataset.training_images.front());
+    REQUIRE(output.size() == 1600);
+
+    auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
+    REQUIRE(result);
+
+    auto test_error = dll::test_set(dbn, dataset.training_images, dataset.training_labels, dll::svm_predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.1);
+}
+
+TEST_CASE("unit/cdbn/mnist/9", "[dbn][conv][mnist][patches][unit]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::patches_layer_desc<14, 14, 14, 14>::layer_t,
+            dll::conv_rbm_desc_square<1, 14, 10, 10, dll::parallel_mode, dll::momentum, dll::batch_size<10>>::layer_t,
+            dll::conv_rbm_desc_square<10, 10, 10, 6, dll::parallel_mode, dll::momentum, dll::batch_size<10>>::layer_t>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(50);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 10);
+
+    auto probs = dbn->activation_probabilities(dataset.training_images[0]);
+    REQUIRE(probs.size() == 4);
+}
+
+TEST_CASE("unit/cdbn/mnist/10", "[dbn][conv][mnist][patches][memory][unit]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::patches_layer_desc<14, 14, 14, 14>::layer_t,
+            dll::conv_rbm_desc_square<1, 14, 20, 10, dll::momentum, dll::batch_size<10>>::layer_t,
+            dll::conv_rbm_desc_square<20, 10, 20, 6, dll::momentum, dll::batch_size<10>>::layer_t>,
+        dll::batch_mode>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(50);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 10);
+
+    auto probs = dbn->activation_probabilities(dataset.training_images[0]);
+    REQUIRE(probs.size() == 4);
+
+    //Simply to ensure compilation
+    if (false) {
+        dbn->display();
+        dbn->store("test.dat");
+        dbn->load("test.dat");
+    }
+}
+
+TEST_CASE("unit/cdbn/mnist/11", "[dbn][conv][mnist][patches][unit]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::patches_layer_padh_desc<14, 14, 14, 14, 1>::layer_t,
+            dll::conv_rbm_desc_square<1, 14, 20, 10, dll::momentum, dll::batch_size<10>>::layer_t,
+            dll::conv_rbm_desc_square<20, 10, 20, 6, dll::momentum, dll::batch_size<10>>::layer_t>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(50);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->pretrain(dataset.training_images, 10);
+
+    auto probs = dbn->activation_probabilities(dataset.training_images[0]);
+    REQUIRE(probs.size() == 4);
+}
+
+TEST_CASE("unit/cdbn/mnist/12", "[dbn][conv][mnist][patches][unit]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::dyn_patches_layer_padh_desc<>::layer_t,
+            dll::dyn_conv_rbm_desc<dll::momentum>::layer_t,
+            dll::dyn_conv_rbm_desc<dll::momentum>::layer_t>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_3d<std::vector, etl::dyn_matrix<double, 3>>(50);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->template init_layer<0>(14, 14, 14, 14, 1);
+    dbn->template init_layer<1>(1, 14, 14, 20, 10, 10);
+    dbn->template init_layer<2>(20, 10, 10, 20, 6, 6);
+
+    dbn->pretrain(dataset.training_images, 10);
+
+    auto probs = dbn->activation_probabilities(dataset.training_images[0]);
+    REQUIRE(probs.size() == 4);
+}
