ed41b5a2bfbaf16c97bdf3e6008259bc993071c0
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-08-29 16:43:23 +0200

    Prepare tests for dbn and types

---
+++ test/src/dbn_types.cpp
@@ -0,0 +1,114 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+/*
+ * This is mostly a compilation test to ensure that DBN is accepting
+ * enough input types
+ */
+
+#include <vector>
+#include <list>
+#include <deque>
+
+#include "catch.hpp"
+#include "template_test.hpp"
+
+#include "dll/rbm.hpp"
+#include "dll/dyn_rbm.hpp"
+#include "dll/dbn.hpp"
+#include "dll/trainer/stochastic_gradient_descent.hpp"
+
+#include "mnist/mnist_reader.hpp"
+#include "mnist/mnist_utils.hpp"
+
+namespace {
+
+struct dbn_double {
+    using dbn_t =
+        dll::dbn_desc<
+            dll::dbn_layers<
+                dll::rbm_desc<28 * 28, 200, dll::weight_type<double>>::layer_t,
+                dll::rbm_desc<200, 10, dll::weight_type<double>>::layer_t
+            >
+            , dll::trainer<dll::sgd_trainer>
+            , dll::batch_size<10>
+            , dll::momentum
+        >::dbn_t;
+
+    static void init(dbn_t& net){
+        net.learning_rate = 0.05;
+        net.initial_momentum = 0.9;
+    }
+};
+
+struct dbn_float {
+    using dbn_t =
+        dll::dbn_desc<
+            dll::dbn_layers<
+                dll::rbm_desc<28 * 28, 200, dll::weight_type<float>>::layer_t,
+                dll::rbm_desc<200, 10, dll::weight_type<float>>::layer_t
+            >
+            , dll::trainer<dll::sgd_trainer>
+            , dll::batch_size<10>
+            , dll::momentum
+        >::dbn_t;
+
+    static void init(dbn_t& net){
+        net.learning_rate = 0.05;
+        net.initial_momentum = 0.9;
+    }
+};
+
+struct dyn_dbn_float {
+    using dbn_t =
+        dll::dbn_desc<
+            dll::dbn_layers<
+                dll::dyn_rbm_desc<dll::weight_type<float>>::layer_t,
+                dll::dyn_rbm_desc<dll::weight_type<float>>::layer_t
+            >
+            , dll::trainer<dll::sgd_trainer>
+            , dll::batch_size<10>
+            , dll::momentum
+        >::dbn_t;
+
+    static void init(dbn_t& net){
+        net.initial_momentum = 0.9;
+        net.learning_rate = 0.05;
+        net.template init_layer<0>(28 * 28, 200);
+        net.template init_layer<1>(200, 10);
+    }
+};
+
+struct dyn_dbn_double {
+    using dbn_t =
+        dll::dbn_desc<
+            dll::dbn_layers<
+                dll::dyn_rbm_desc<dll::weight_type<double>>::layer_t,
+                dll::dyn_rbm_desc<dll::weight_type<double>>::layer_t
+            >
+            , dll::trainer<dll::sgd_trainer>
+            , dll::batch_size<10>
+            , dll::momentum
+        >::dbn_t;
+
+    static void init(dbn_t& net){
+        net.initial_momentum = 0.9;
+        net.learning_rate = 0.05;
+        net.template init_layer<0>(28 * 28, 200);
+        net.template init_layer<1>(200, 10);
+    }
+};
+
+} // end of anonymous namespace
+
+#define TYPES_TEST_PREFIX "dbn"
+#define TYPES_TEST_T1 dbn_float
+#define TYPES_TEST_T2 dbn_double
+#define TYPES_TEST_T3 dyn_dbn_float
+#define TYPES_TEST_T4 dyn_dbn_double
+
+#include "dbn_types_test.inl"
