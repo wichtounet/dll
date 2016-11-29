d948f138193a396f95b14e0e0712952f8951fbaf
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-11-29 16:26:09 +0100

    New test

--- test/src/rbm_relu.cpp
+++ test/src/rbm_relu.cpp
@@ -61,3 +61,27 @@ TEST_CASE("rbm/mnist_11", "rbm::relu6") {
 
     REQUIRE(error < 1e-1);
 }
+
+TEST_CASE("rbm/relu/1", "[relu][rbm][clip]") {
+    using rbm_t = dll::rbm_desc<
+            28 * 28, 100
+            , dll::momentum
+            , dll::batch_size<25>
+            , dll::hidden<dll::unit_type::RELU>
+            , dll::clip_gradients
+        >::layer_t;
+
+    rbm_t rbm;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::dyn_vector<float>>(500);
+    REQUIRE(!dataset.training_images.empty());
+
+    mnist::binarize_dataset(dataset);
+
+    rbm.initial_momentum = 0.9;
+    rbm.momentum = 0.9;
+
+    auto error = rbm.train(dataset.training_images, 200);
+
+    REQUIRE(error < 1e-1);
+}
