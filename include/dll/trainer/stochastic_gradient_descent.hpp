49b2051d9b33929df1c2541c7dfdf7caaea98a50
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-01-10 12:57:27 +0100

    Review SGD implementation
    The implementation is not independent of the layers.
    It is up to the layers to perform the gradients and derivatives.
    It is up to the layers to adapt to other layers configuration.

--- include/dll/trainer/stochastic_gradient_descent.hpp
+++ include/dll/trainer/stochastic_gradient_descent.hpp
@@ -43,34 +43,6 @@ struct extract_function<L, std::enable_if_t<decay_layer_traits<L>::is_rbm_layer(
             : (std::decay_t<L>::hidden_unit == unit_type::SOFTMAX ? function::SOFTMAX : function::RELU);
 };
 
-template <typename Layer, typename Input, typename Output, typename Errors, cpp_enable_if(decay_layer_traits<Layer>::is_max_pooling_layer() && !decay_layer_traits<Layer>::is_dynamic())>
-auto upsample(Input&& input, Output&& output, Errors&& errors) {
-    return etl::max_pool_derivative_3d<Layer::C1, Layer::C2, Layer::C3>(input, output) >> etl::upsample_3d<Layer::C1, Layer::C2, Layer::C3>(errors);
-}
-
-template <typename Layer, typename Input, typename Output, typename Errors, cpp_enable_if(decay_layer_traits<Layer>::is_max_pooling_layer() && decay_layer_traits<Layer>::is_dynamic())>
-auto upsample(Input&& input, Output&& output, Errors&& errors) {
-    auto c1 = etl::dim(input, 0) / etl::dim(output, 0);
-    auto c2 = etl::dim(input, 1) / etl::dim(output, 1);
-    auto c3 = etl::dim(input, 2) / etl::dim(output, 2);
-
-    return etl::max_pool_derivative_3d(input, output, c1, c2, c3) >> etl::upsample_3d(errors, c1, c2, c3);
-}
-
-template <typename Layer, typename Input, typename Output, typename Errors, cpp_enable_if(decay_layer_traits<Layer>::is_avg_pooling_layer() && !decay_layer_traits<Layer>::is_dynamic())>
-auto upsample(Input&& input, Output&& output, Errors&& errors) {
-    return etl::avg_pool_derivative_3d<Layer::C1, Layer::C2, Layer::C3>(input, output) >> etl::upsample_3d<Layer::C1, Layer::C2, Layer::C3>(errors);
-}
-
-template <typename Layer, typename Input, typename Output, typename Errors, cpp_enable_if(decay_layer_traits<Layer>::is_avg_pooling_layer() && decay_layer_traits<Layer>::is_dynamic())>
-auto upsample(Input&& input, Output&& output, Errors&& errors) {
-    auto c1 = etl::dim(input, 0) / etl::dim(output, 0);
-    auto c2 = etl::dim(input, 1) / etl::dim(output, 1);
-    auto c3 = etl::dim(input, 2) / etl::dim(output, 2);
-
-    return etl::avg_pool_derivative_3d(input, output, c1, c2, c3) >> etl::upsample_3d(errors, c1, c2, c3);
-}
-
 template <typename DBN>
 struct sgd_trainer {
     using dbn_t     = DBN;
@@ -156,147 +128,6 @@ struct sgd_trainer {
 
     void init_training(std::size_t) {}
 
-    template <typename Sample>
-    void compute_outputs(const Sample& item_data) {
-        auto& first_layer         = dbn.template layer_get<0>();
-        auto& first_layer_context = first_layer.template get_sgd_context<dbn_t>();
-
-        first_layer.batch_activate_hidden(first_layer_context.output, item_data);
-
-        dbn.for_each_layer_pair([](auto& layer_1, auto& layer_2) {
-            auto& ctx1 = layer_1.template get_sgd_context<dbn_t>();
-            auto& ctx2 = layer_2.template get_sgd_context<dbn_t>();
-
-            layer_2.batch_activate_hidden(ctx2.output, ctx1.output);
-
-            cpp::static_if<decay_layer_traits<decltype(layer_2)>::is_pooling_layer()>([&](auto f) {
-                f(ctx2).input = ctx1.output;
-            });
-        });
-    }
-
-    template <typename Layer, typename Weight, typename Grad, typename Inputs, typename Errors,
-             cpp_enable_if(decay_layer_traits<Layer>::is_dense_layer() && etl::decay_traits<Inputs>::dimensions() == 2)>
-    static void compute_weight_gradients(Layer&, Grad& grad, Inputs& inputs, Errors& errors) {
-        dense_compute_weight_gradients<Weight>(grad, inputs, errors);
-    }
-
-    template <typename Layer, typename Weight, typename Grad, typename Inputs, typename Errors,
-             cpp_enable_if(decay_layer_traits<Layer>::is_dense_layer() && etl::decay_traits<Inputs>::dimensions() != 2)>
-    static void compute_weight_gradients(Layer& layer, Grad& grad, Inputs& inputs, Errors& errors) {
-        dense_compute_weight_gradients<Weight>(grad, etl::reshape(inputs, batch_size, num_visible(layer)), errors);
-    }
-
-    template <typename Weight, typename Grad, typename Inputs, typename Errors>
-    static void dense_compute_weight_gradients(Grad& grad, Inputs&& inputs, Errors& errors) {
-        // Note: This is the point that takes the more in SGD training
-        grad = batch_outer(inputs, errors);
-    }
-
-    template <typename Layer, typename Weight, typename Grad, typename Inputs, typename Errors,
-             cpp_enable_if(decay_layer_traits<Layer>::is_convolutional_layer())>
-    static void compute_weight_gradients(Layer& /*layer*/, Grad& grad, Inputs& inputs, Errors& errors) {
-        grad = conv_4d_valid_filter_flipped(inputs, errors);
-    }
-
-    template <typename Layer, typename Context, typename Inputs,
-             cpp_enable_if((decay_layer_traits<Layer>::is_neural_layer()))>
-    static void compute_gradients(Layer& layer, Context& ctx, Inputs& inputs) {
-        ctx.w_grad = 0;
-
-        compute_weight_gradients<Layer, weight>(layer, ctx.w_grad, inputs, ctx.errors);
-
-        cpp::static_if<decay_layer_traits<Layer>::is_dense_layer()>([&](auto f) {
-            f(ctx.b_grad) = etl::sum_l(ctx.errors);
-        }).else_([&](auto f) {
-            f(ctx.b_grad) = etl::mean_r(etl::sum_l(f(ctx.errors)));
-        });
-
-        nan_check_deep(ctx.w_grad);
-        nan_check_deep(ctx.b_grad);
-    }
-
-    template <typename Layer, typename Context, typename Inputs, cpp_enable_if(decay_layer_traits<Layer>::is_pooling_layer() || decay_layer_traits<Layer>::is_transform_layer())>
-    static void compute_gradients(Layer&, Context&, Inputs&) {
-        //Pooling and transform layers have no weight
-    }
-
-    //Backpropagate errors from pooling
-    template <typename Layer1, typename Layer2, cpp_enable_if(decay_layer_traits<Layer2>::is_pooling_layer())>
-    static void compute_errors(Layer1& r1, Layer2& r2) {
-        constexpr const auto a_f = extract_function<Layer1>::activation_function;
-
-        auto& ctx1 = r1.template get_sgd_context<dbn_t>();
-        auto& ctx2 = r2.template get_sgd_context<dbn_t>();
-
-        for (std::size_t i = 0; i < batch_size; ++i) {
-            ctx1.errors(i) = f_derivative<a_f>(ctx1.output(i)) >> upsample<Layer2>(ctx2.input(i), ctx2.output(i), ctx2.errors(i));
-        }
-
-        nan_check_deep(ctx1.errors);
-    }
-
-    template <typename Layer1, typename Context1, typename Layer2, typename Context2, typename DF>
-    static void compute_errors_from_dense(Layer1&, Context1& ctx1, Layer2& r2, Context2& ctx2, DF derivative) {
-        ctx1.errors = derivative() >> ctx2.errors * etl::transpose(r2.w);
-
-        nan_check_deep(ctx1.errors);
-    }
-
-    template <typename Layer1, typename Context1, typename Layer2, typename Context2, typename DF>
-    static void compute_errors_from_conv(Layer1&, Context1& ctx1, Layer2& r2, Context2& ctx2, DF derivative) {
-        ctx1.errors = derivative() >> etl::conv_4d_full_flipped(ctx2.errors, r2.w);
-
-        nan_check_deep(ctx1.errors);
-    }
-
-    //Backpropagate errors from dense to (dense or conv)
-    template <typename Layer1, typename Layer2,
-             cpp_enable_if(decay_layer_traits<Layer1>::is_neural_layer() && decay_layer_traits<Layer2>::is_dense_layer())>
-    static void compute_errors(Layer1& r1, Layer2& r2) {
-        constexpr const auto a_f = extract_function<Layer1>::activation_function;
-
-        auto& ctx1 = r1.template get_sgd_context<dbn_t>();
-        auto& ctx2 = r2.template get_sgd_context<dbn_t>();
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wuninitialized"
-        compute_errors_from_dense(r1, ctx1, r2, ctx2, [&] { return f_derivative<a_f>(ctx1.output); });
-#pragma GCC diagnostic pop
-    }
-
-    //Backpropagate errors from conv to (dense or conv)
-    template <typename Layer1, typename Layer2,
-             cpp_enable_if(decay_layer_traits<Layer1>::is_neural_layer() && decay_layer_traits<Layer2>::is_convolutional_layer())>
-    static void compute_errors(Layer1& r1, Layer2& r2) {
-        constexpr const auto a_f = extract_function<Layer1>::activation_function;
-
-        auto& ctx1 = r1.template get_sgd_context<dbn_t>();
-        auto& ctx2 = r2.template get_sgd_context<dbn_t>();
-
-        compute_errors_from_conv(r1, ctx1, r2, ctx2, [&] { return f_derivative<a_f>(ctx1.output); });
-    }
-
-    //Backpropagate errors from dense to pooling
-    template <typename Layer1, typename Layer2,
-             cpp_enable_if(!decay_layer_traits<Layer1>::is_neural_layer() && decay_layer_traits<Layer2>::is_dense_layer())>
-    static void compute_errors(Layer1& r1, Layer2& r2) {
-        auto& ctx1 = r1.template get_sgd_context<dbn_t>();
-        auto& ctx2 = r2.template get_sgd_context<dbn_t>();
-
-        compute_errors_from_dense(r1, ctx1, r2, ctx2, [] { return 1.0; });
-    }
-
-    //Backpropagate errors from conv to pooling
-    template <typename Layer1, typename Layer2,
-             cpp_enable_if(!decay_layer_traits<Layer1>::is_neural_layer() && decay_layer_traits<Layer2>::is_convolutional_layer())>
-    static void compute_errors(Layer1& r1, Layer2& r2) {
-        auto& ctx1 = r1.template get_sgd_context<dbn_t>();
-        auto& ctx2 = r2.template get_sgd_context<dbn_t>();
-
-        compute_errors_from_conv(r1, ctx1, r2, ctx2, [] { return 1.0; });
-    }
-
     template <typename D, typename It>
     void copy_inputs(D& dest, It first, It last) {
         std::size_t i = 0;
@@ -319,26 +150,6 @@ struct sgd_trainer {
         }
     }
 
-    template <typename Layer, typename Context, typename Labels, cpp_enable_if(decay_layer_traits<Layer>::is_standard_dense_layer())>
-    void compute_last_errors(Layer& /*layer*/, Context& context, Labels& labels) {
-        constexpr const auto last_a_f = extract_function<Layer>::activation_function;
-
-        context.errors = f_derivative<last_a_f>(context.output) >> (labels - context.output);
-
-        nan_check_deep(context.errors);
-    }
-
-    template <typename Layer, typename Context, typename Labels, cpp_enable_if(decay_layer_traits<Layer>::is_dense_rbm_layer())>
-    void compute_last_errors(Layer& /*layer*/, Context& context, Labels& labels) {
-        constexpr const auto last_a_f = extract_function<Layer>::activation_function;
-
-        static_assert(last_a_f == function::SOFTMAX || last_a_f == function::SIGMOID || last_a_f == function::RELU, "Only softmax/sigmoid/relu RBM can be used as last RBM layer");
-
-        context.errors = f_derivative<last_a_f>(context.output) >> (labels - context.output);
-
-        nan_check_deep(context.errors);
-    }
-
     template <typename T, typename L, typename InputTransformer>
     std::pair<double, double> train_batch(std::size_t /*epoch*/, const dll::batch<T>& data_batch, const dll::batch<L>& label_batch, InputTransformer input_transformer) {
         cpp_assert(data_batch.size() == label_batch.size(), "Invalid sizes");
@@ -353,6 +164,10 @@ struct sgd_trainer {
 
         decltype(auto) last_layer = dbn.template layer_get<layers - 1>();
         decltype(auto) last_ctx = last_layer.template get_sgd_context<dbn_t>();
+
+        static_assert(
+            decay_layer_traits<decltype(last_layer)>::is_dense_layer(),
+            "The last layer must be dense for SGD trainining");
 
         // Prepare initial inputs and final outputs (labels)
 
@@ -371,34 +186,44 @@ struct sgd_trainer {
 
         //Feedforward pass
 
-        compute_outputs(tilde_inputs);
+        first_ctx.input = tilde_inputs;
+        first_layer.batch_activate_hidden(first_ctx.output, first_ctx.input);
 
-        static_assert(
-            decay_layer_traits<decltype(last_layer)>::is_dense_layer(),
-            "The last layer must be dense for SGD trainining");
+        dbn.for_each_layer_pair([](auto& layer_1, auto& layer_2) {
+            auto& ctx1 = layer_1.template get_sgd_context<dbn_t>();
+            auto& ctx2 = layer_2.template get_sgd_context<dbn_t>();
+
+            ctx2.input = ctx1.output;
+            layer_2.batch_activate_hidden(ctx2.output, ctx2.input);
+        });
 
         //Compute the errors of the last layer
 
-        compute_last_errors(last_layer, last_ctx, labels);
+        last_ctx.errors = labels - last_ctx.output;
 
-        //Compute the gradients of each layer
+        // Backpropagate the error
 
         dbn.for_each_layer_rpair([](auto& r1, auto& r2) {
             auto& ctx1 = r1.template get_sgd_context<dbn_t>();
             auto& ctx2 = r2.template get_sgd_context<dbn_t>();
 
-            this_type::compute_gradients(r2, ctx2, ctx1.output);
-
-            this_type::compute_errors(r1, r2);
+            r2.adapt_errors(ctx2);
+            r2.backward_batch(ctx1.errors, ctx2);
         });
 
-        compute_gradients(first_layer, first_ctx, inputs);
+        first_layer.adapt_errors(first_ctx);
 
-        //Apply gradients
+        // Compute and apply the gradients
 
         dbn.for_each_layer([this, n](auto& layer) {
+            // Compute the gradients
+            layer.compute_gradients(layer.template get_sgd_context<dbn_t>());
+
+            // Apply the gradients
             this->apply_gradients(layer, n);
         });
+
+        // Compute error and loss
 
         double error = etl::mean(etl::abs(labels - last_ctx.output));
         double loss = -etl::sum(etl::log(last_ctx.output) >> labels) / (double) n;
