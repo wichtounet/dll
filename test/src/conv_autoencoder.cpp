94013ad405b832f54a2b411c98a3d32e2263068f
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-01-19 08:13:42 +0100

    Test for upsample

--- test/src/conv_autoencoder.cpp
+++ test/src/conv_autoencoder.cpp
@@ -11,6 +11,8 @@
 
 #include "dll/neural/conv_layer.hpp"
 #include "dll/neural/deconv_layer.hpp"
+#include "dll/pooling/mp_layer.hpp"
+#include "dll/pooling/upsample_layer.hpp"
 #include "dll/dbn.hpp"
 #include "dll/trainer/stochastic_gradient_descent.hpp"
 
@@ -21,6 +23,36 @@ TEST_CASE("conv/ae/1", "[dense][dbn][mnist][sgd][ae]") {
     typedef dll::dbn_desc<
         dll::dbn_layers<
             dll::conv_desc<1, 28, 28, 10, 24, 24, dll::activation<dll::function::SIGMOID>>::layer_t,
+            dll::deconv_desc<10, 24, 24, 1, 5, 5, dll::activation<dll::function::SIGMOID>>::layer_t
+        >, dll::trainer<dll::sgd_trainer>, dll::batch_size<20>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(1000);
+    REQUIRE(!dataset.training_images.empty());
+
+    dll_test::mnist_scale(dataset);
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->display();
+
+    dbn->learning_rate = 0.1;
+
+    auto ft_error = dbn->fine_tune_ae(dataset.training_images, 50);
+    std::cout << "ft_error:" << ft_error << std::endl;
+
+    CHECK(ft_error < 5e-2);
+
+    auto test_error = dll::test_set_ae(*dbn, dataset.test_images);
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.1);
+}
+
+TEST_CASE("conv/ae/2", "[dense][dbn][mnist][sgd][ae]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_desc<1, 28, 28, 10, 24, 24, dll::activation<dll::function::SIGMOID>>::layer_t,
+            dll::mp_layer_3d_desc<10, 24, 24, 1, 2, 2>::layer_t,
+            dll::upsample_layer_3d_desc<10, 12, 12, 1, 2, 2>::layer_t,
             dll::deconv_desc<10, 24, 24, 1, 5, 5, dll::activation<dll::function::SIGMOID>>::layer_t
         >, dll::trainer<dll::sgd_trainer>, dll::batch_size<20>>::dbn_t dbn_t;
 
