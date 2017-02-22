2d17140b678d32fea1ab9abe96d0807d54ca71c8
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-02-22 20:04:13 +0100

    Test with denoising auto and batch mode

--- workbench/src/rbm_dae.cpp
+++ workbench/src/rbm_dae.cpp
@@ -34,6 +34,27 @@ void rbm_dae(const D& dataset){
     ae->pretrain_denoising_auto(dataset.training_images, 50, 0.3);
 }
 
+template<typename D>
+void rbm_dae_batch(const D& dataset){
+    std::cout << " Test RBM Denoising Auto-Encoder" << std::endl;
+
+    using network_t = dll::dbn_desc<
+        dll::dbn_layers<
+            dll::rbm_desc<28 * 28, 100, dll::momentum, dll::batch_size<25>>::layer_t
+        >,
+        dll::batch_size<50>,
+        dll::batch_mode>::dbn_t;
+
+    auto ae = std::make_unique<network_t>();
+
+    ae->display();
+
+    ae->template layer_get<0>().learning_rate = 0.001;
+    ae->template layer_get<0>().initial_momentum = 0.9;
+
+    ae->pretrain_denoising_auto(dataset.training_images, 50, 0.3);
+}
+
 } //end of anonymous namespace
 
 int main(int /*argc*/, char* /*argv*/ []) {
@@ -44,6 +65,7 @@ int main(int /*argc*/, char* /*argv*/ []) {
     std::cout << n << " samples to test" << std::endl;
 
     mnist::binarize_dataset(dataset);
+    rbm_dae_batch(dataset);
     rbm_dae(dataset);
 
     return 0;
