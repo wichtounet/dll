e5003155035bcbbc02713bb5b032986e74d3dd79
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-11 12:17:08 +0100

    Add Catch main

---
+++ test/src/unit/test.cpp
@@ -0,0 +1,9 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
