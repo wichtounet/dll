902860fc1ad932e2df1a2f5e107516fd8612ca3e
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-13 12:45:10 +0100

    Make the tests more useful

--- workbench/src/compile_dyn_rbm_one.cpp
+++ workbench/src/compile_dyn_rbm_one.cpp
@@ -20,6 +20,8 @@
 int main(int, char**) {
     auto dataset = mnist::read_dataset_direct<std::vector, etl::dyn_vector<float>>();
 
+    mnist::binarize_dataset(dataset);
+
     using dbn3_t =
         dll::dbn_desc<
             dll::dbn_layers<
@@ -30,14 +32,14 @@ int main(int, char**) {
 
 #define decl_dbn3(NAME, F)                                      \
     auto NAME = std::make_unique<dbn3_t>();                     \
-    NAME->template layer_get<0>().init_layer(28 * 28, 100 + F); \
-    NAME->template layer_get<1>().init_layer(100 + F, 200 + F); \
-    NAME->template layer_get<2>().init_layer(200 + F, 10);      \
+    NAME->template layer_get<0>().init_layer(28 * 28, 500 + F); \
+    NAME->template layer_get<1>().init_layer(500 + F, 400 + F); \
+    NAME->template layer_get<2>().init_layer(400 + F, 10);      \
     NAME->template layer_get<0>().batch_size = 64;              \
     NAME->template layer_get<1>().batch_size = 64;              \
     NAME->template layer_get<2>().batch_size = 64;              \
-    NAME->pretrain(dataset.training_images, 20);                \
-    NAME->fine_tune(dataset.training_images, dataset.training_labels, 20);
+    NAME->pretrain(dataset.training_images, 10);                \
+    NAME->fine_tune(dataset.training_images, dataset.training_labels, 10);
 
     decl_dbn3(dbn1,1)
 
