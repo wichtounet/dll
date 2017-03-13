38f63b83cb6cabba0ff9d30a68e3a09a380fd505
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-13 10:44:17 +0100

    Integrate SGD

--- workbench/src/compile_dyn_rbm_one.cpp
+++ workbench/src/compile_dyn_rbm_one.cpp
@@ -9,6 +9,7 @@
 #include <chrono>
 
 #include "dll/rbm/dyn_rbm.hpp"
+#include "dll/trainer/stochastic_gradient_descent.hpp"
 #include "dll/dbn.hpp"
 
 #include "mnist/mnist_reader.hpp"
@@ -24,14 +25,19 @@ int main(int, char**) {
             dll::dbn_layers<
                 dll::dyn_rbm_desc<dll::momentum>::layer_t,
                 dll::dyn_rbm_desc<dll::momentum>::layer_t,
-                dll::dyn_rbm_desc<dll::momentum, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>>::dbn_t;
+                dll::dyn_rbm_desc<dll::momentum, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>,
+            dll::batch_size<64>, dll::trainer<dll::sgd_trainer>>::dbn_t;
 
-#define decl_dbn3(NAME, F) \
-    auto NAME = std::make_unique<dbn3_t>(); \
-    NAME->template layer_get<0>().init_layer(28 * 28, 100 + F);\
-    NAME->template layer_get<1>().init_layer(100 + F, 200 + F);\
-    NAME->template layer_get<2>().init_layer(200 + F, 10);\
-    NAME->pretrain(dataset.training_images, 20);
+#define decl_dbn3(NAME, F)                                      \
+    auto NAME = std::make_unique<dbn3_t>();                     \
+    NAME->template layer_get<0>().init_layer(28 * 28, 100 + F); \
+    NAME->template layer_get<1>().init_layer(100 + F, 200 + F); \
+    NAME->template layer_get<2>().init_layer(200 + F, 10);      \
+    NAME->template layer_get<0>().batch_size = 64;              \
+    NAME->template layer_get<1>().batch_size = 64;              \
+    NAME->template layer_get<2>().batch_size = 64;              \
+    NAME->pretrain(dataset.training_images, 20);                \
+    NAME->fine_tune(dataset.training_images, dataset.training_labels, 20);
 
     decl_dbn3(dbn1,1)
 
--- workbench/src/compile_hybrid_rbm.cpp
+++ workbench/src/compile_hybrid_rbm.cpp
@@ -9,6 +9,7 @@
 #include <chrono>
 
 #include "dll/rbm/rbm.hpp"
+#include "dll/trainer/stochastic_gradient_descent.hpp"
 #include "dll/dbn.hpp"
 
 #include "mnist/mnist_reader.hpp"
@@ -19,15 +20,17 @@
 int main(int, char**) {
     auto dataset = mnist::read_dataset_direct<std::vector, etl::dyn_vector<float>>();
 
-#define decl_dbn3(NAME, NAME_T, F) \
-    using NAME_T = \
-        dll::dyn_dbn_desc< \
-            dll::dbn_layers< \
-                dll::rbm_desc<28*28, 100 + F, dll::momentum>::layer_t, \
-                dll::rbm_desc<100 + F, 200 + F, dll::momentum>::layer_t, \
-                dll::rbm_desc<200 + F, 10, dll::momentum, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>>::dbn_t; \
-    auto NAME = std::make_unique<NAME_T>(); \
-    NAME->pretrain(dataset.training_images, 20);
+#define decl_dbn3(NAME, NAME_T, F)                                                                                              \
+    using NAME_T =                                                                                                              \
+        dll::dyn_dbn_desc<                                                                                                      \
+            dll::dbn_layers<                                                                                                    \
+                dll::rbm_desc<28 * 28, 100 + F, dll::momentum, dll::batch_size<64>>::layer_t,                                   \
+                dll::rbm_desc<100 + F, 200 + F, dll::momentum, dll::batch_size<64>>::layer_t,                                   \
+                dll::rbm_desc<200 + F, 10, dll::momentum, dll::batch_size<64>, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>, \
+            dll::trainer<dll::sgd_trainer>, dll::batch_size<64>>::dbn_t;                                                        \
+    auto NAME = std::make_unique<NAME_T>();                                                                                     \
+    NAME->pretrain(dataset.training_images, 20);                                                                                \
+    NAME->fine_tune(dataset.training_images, dataset.training_labels, 20);
 
     decl_dbn3(dbn1,dbn1_t,1)
     decl_dbn3(dbn2,dbn2_t,2)
