a61d436598dd1074214633da80ff0cde0ba37cdd
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-13 15:46:46 +0100

    Add support for automatic hybrid compilation

--- include/dll/dbn_desc.hpp
+++ include/dll/dbn_desc.hpp
@@ -11,9 +11,6 @@
 
 namespace dll {
 
-template <typename Layers, typename... Parameters>
-using dbn_desc = generic_dbn_desc<dbn, Layers, Parameters...>;
-
 template <typename Layers>
 struct dyn_layers_t;
 
@@ -34,5 +31,22 @@ struct generic_dyn_dbn_desc : generic_dbn_desc<DBN_T, Layers, Parameters...> {
 
 template <typename Layers, typename... Parameters>
 using dyn_dbn_desc = generic_dyn_dbn_desc<dbn, Layers, Parameters...>;
+
+// By default dbn_desc use directly the layers that it is provided
+// if DLL_QUICK is set, it is set to hybrid mode by default
+
+#ifndef DLL_QUICK
+template <typename Layers, typename... Parameters>
+using dbn_desc = generic_dbn_desc<dbn, Layers, Parameters...>;
+#else
+template <typename Layers, typename... Parameters>
+using dbn_desc = generic_dyn_dbn_desc<dbn, Layers, Parameters...>;
+#endif
+
+// fast_dbn_desc is always forced to direct mode, will not respect
+// DLL_QUICK
+
+template <typename Layers, typename... Parameters>
+using fast_dbn_desc = generic_dbn_desc<dbn, Layers, Parameters...>;
 
 } //end of dll namespace
