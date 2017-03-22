34b78636794db86a21c8b9ba0924f2065f27f577
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-03-22 15:28:58 +0100

    Third experiment

--- workbench/src/conv_sgd_perf.cpp
+++ workbench/src/conv_sgd_perf.cpp
@@ -94,6 +94,44 @@ void second_ex(){
     etl::dump_counters();
 }
 
+void third_ex(){
+    // Third experiment : Conv -> Pooling -> Conv -> Pooling -> Dense -> Dense
+    // Current speed on frigg: 15-17 seconds
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(3000);
+
+    auto n = dataset.training_images.size();
+    std::cout << n << " samples to test" << std::endl;
+
+    mnist::binarize_dataset(dataset);
+
+    // Clean slate
+    etl::reset_counters();
+    dll::reset_timers();
+
+    using dbn_t = dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_desc<1, 28, 28, 10, 5, 5>::layer_t,
+            dll::mp_layer_3d_desc<10, 24, 24, 1, 2, 2>::layer_t,
+            dll::conv_desc<10, 12, 12, 10, 5, 5>::layer_t,
+            dll::mp_layer_3d_desc<10, 8, 8, 1, 2, 2>::layer_t,
+            dll::dense_desc<10 * 4 * 4, 200>::layer_t,
+            dll::dense_desc<200, 10, dll::activation<dll::function::SOFTMAX>>::layer_t>,
+        dll::momentum, dll::batch_size<100>, dll::trainer<dll::sgd_trainer>>::dbn_t;
+
+    auto net = std::make_unique<dbn_t>();
+
+    // Train the network for performance sake
+    net->display();
+    net->fine_tune(dataset.training_images, dataset.training_labels, 20);
+
+    std::cout << "DLL Timers" << std::endl;
+    dll::dump_timers();
+
+    std::cout << "ETL Counters" << std::endl;
+    etl::dump_counters();
+}
+
 } // end of anonymous namespace
 
 int main(int argc, char* argv []) {
@@ -106,6 +144,8 @@ int main(int argc, char* argv []) {
         first_ex();
     } else if(select == "B"){
         second_ex();
+    } else if(select == "C"){
+        third_ex();
     }
 
     return 0;
