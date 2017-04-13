de2cb7434e1d8f8da173e71d26f909d1b1c42320
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-04-13 13:53:25 +0200

    Test for partial training

--- test/src/unit/unit_conv_1.cpp
+++ test/src/unit/unit_conv_1.cpp
@@ -118,3 +118,51 @@ TEST_CASE("unit/conv/sgd/5", "[conv][dbn][mnist][sgd]") {
     FT_CHECK(25, 6e-2);
     TEST_CHECK(0.2);
 }
+
+// Test custom training
+TEST_CASE("unit/conv/sgd/partial/1", "[conv][dbn][mnist][sgd]") {
+    typedef dll::dbn_desc<
+        dll::dbn_layers<
+            dll::conv_desc<1, 28, 28, 6, 5, 5, dll::activation<dll::function::SIGMOID>>::layer_t,
+            dll::dense_desc<6 * 24 * 24, 10, dll::activation<dll::function::SIGMOID>>::layer_t>,
+        dll::trainer<dll::sgd_trainer>, dll::batch_size<10>>::dbn_t dbn_t;
+
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(350);
+    REQUIRE(!dataset.training_images.empty());
+
+    auto dbn = std::make_unique<dbn_t>();
+
+    dbn->learning_rate = 0.07;
+
+    auto trainer = dbn->get_trainer();
+
+    trainer.start_training(*dbn, false, 25);
+
+    // Train for 25 epochs
+    for(size_t epoch = 0; epoch < 25; ++epoch){
+        trainer.start_epoch(*dbn, epoch);
+
+        double error = 0.0;
+        double loss = 0.0;
+
+        for(std::size_t i = 0; i < 7; ++i){
+            std::tie(loss, error) = trainer.train_partial(
+                *dbn, false,
+                dataset.training_images.begin() + i * 50, dataset.training_images.begin() + (i + 1) * 50,
+                dataset.training_labels.begin() + i * 50,
+                epoch);
+        }
+
+        if(trainer.stop_epoch(*dbn, epoch, error, loss)){
+            break;
+        }
+    }
+
+    auto ft_error = trainer.stop_training(*dbn);
+
+    REQUIRE(ft_error < 5e-2);
+
+    auto test_error = dll::test_set(dbn, dataset.test_images, dataset.test_labels, dll::predictor());
+    std::cout << "test_error:" << test_error << std::endl;
+    REQUIRE(test_error < 0.2);
+}
