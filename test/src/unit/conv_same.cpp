818bf2a49d718b06cf24140b9fa664f974faa85b
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-05-16 11:19:29 +0200

    Cleanup the tests

--- test/src/unit/conv_same.cpp
+++ test/src/unit/conv_same.cpp
@@ -46,7 +46,7 @@ TEST_CASE("unit/conv/same/2", "[conv][dbn][mnist][sgd]") {
             dll::dense_desc<6 * 14 * 14, 10, dll::activation<dll::function::SOFTMAX>>::layer_t>,
         dll::trainer<dll::sgd_trainer>, dll::batch_size<20>>::dbn_t dbn_t;
 
-    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(600);
     REQUIRE(!dataset.training_images.empty());
 
     auto dbn = std::make_unique<dbn_t>();
@@ -55,60 +55,6 @@ TEST_CASE("unit/conv/same/2", "[conv][dbn][mnist][sgd]") {
 
     dbn->display();
 
-    FT_CHECK(25, 5e-2);
-    TEST_CHECK(0.2);
-}
-
-TEST_CASE("unit/conv/same/3", "[conv][dbn][mnist][sgd]") {
-    typedef dll::dbn_desc<
-        dll::dbn_layers<
-            dll::conv_same_desc<1, 28, 28, 6, 3, 3, dll::activation<dll::function::RELU>>::layer_t,
-            dll::mp_layer_3d_desc<6, 28, 28, 1, 2, 2>::layer_t,
-
-            dll::conv_same_desc<6, 14, 14, 12, 3, 3, dll::activation<dll::function::RELU>>::layer_t,
-            dll::mp_layer_3d_desc<12, 14, 14, 1, 2, 2>::layer_t,
-
-            dll::dense_desc<12 * 7 * 7, 400, dll::activation<dll::function::RELU>>::layer_t,
-            dll::dense_desc<400, 10, dll::activation<dll::function::SOFTMAX>>::layer_t
-        >, dll::trainer<dll::sgd_trainer>, dll::batch_size<20>>::dbn_t dbn_t;
-
-    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
-    REQUIRE(!dataset.training_images.empty());
-
-    auto dbn = std::make_unique<dbn_t>();
-
-    dbn->learning_rate = 0.01;
-
-    dbn->display();
-
-    FT_CHECK(25, 5e-2);
-    TEST_CHECK(0.2);
-}
-
-TEST_CASE("unit/conv/same/4", "[conv][dbn][mnist][sgd]") {
-    typedef dll::dbn_desc<
-        dll::dbn_layers<
-            dll::conv_same_desc<1, 28, 28, 6, 3, 3, dll::activation<dll::function::RELU>>::layer_t,
-            dll::conv_same_desc<6, 28, 28, 6, 3, 3, dll::activation<dll::function::RELU>>::layer_t,
-            dll::mp_layer_3d_desc<6, 28, 28, 1, 2, 2>::layer_t,
-
-            dll::conv_same_desc<6, 14, 14, 12, 3, 3, dll::activation<dll::function::RELU>>::layer_t,
-            dll::conv_same_desc<12, 14, 14, 12, 3, 3, dll::activation<dll::function::RELU>>::layer_t,
-            dll::mp_layer_3d_desc<12, 14, 14, 1, 2, 2>::layer_t,
-
-            dll::dense_desc<12 * 7 * 7, 400, dll::activation<dll::function::RELU>>::layer_t,
-            dll::dense_desc<400, 10, dll::activation<dll::function::SOFTMAX>>::layer_t
-        >, dll::trainer<dll::sgd_trainer>, dll::batch_size<20>>::dbn_t dbn_t;
-
-    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
-    REQUIRE(!dataset.training_images.empty());
-
-    auto dbn = std::make_unique<dbn_t>();
-
-    dbn->learning_rate = 0.01;
-
-    dbn->display();
-
-    FT_CHECK(25, 5e-2);
+    FT_CHECK(50, 5e-2);
     TEST_CHECK(0.2);
 }
