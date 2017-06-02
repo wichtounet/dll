840c1a879e9408e6f83879f657f977e1a51c3ad7
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-06-02 15:20:45 +0200

    Doc

--- include/dll/datasets.hpp
+++ include/dll/datasets.hpp
@@ -13,6 +13,13 @@
 
 namespace dll {
 
+/*!
+ * \brief Create a data generator around the MNIST train set
+ * \param folder The folder in which the MNIST train files are
+ * \param batch The batch size (0 = default from parameters)
+ * \param parameters The parameters of the generator
+ * \return a unique_ptr around the create generator
+ */
 template<typename... Parameters>
 auto make_mnist_generator_train(const std::string& folder, size_t batch = 0, Parameters&&... /*parameters*/){
     // Create examples for the caches
@@ -41,6 +48,13 @@ auto make_mnist_generator_train(const std::string& folder, size_t batch = 0, Par
     return generator;
 }
 
+/*!
+ * \brief Create a data generator around the MNIST test set
+ * \param folder The folder in which the MNIST test files are
+ * \param batch The batch size (0 = default from parameters)
+ * \param parameters The parameters of the generator
+ * \return a unique_ptr around the create generator
+ */
 template<typename... Parameters>
 auto make_mnist_generator_test(const std::string& folder, size_t batch = 0, Parameters&&... /*parameters*/){
     // Create examples for the caches
@@ -69,34 +83,77 @@ auto make_mnist_generator_test(const std::string& folder, size_t batch = 0, Para
     return generator;
 }
 
+/*!
+ * \brief Create a data generator around the MNIST train set.
+ *
+ * The MNIST train files are assumed to be in a mnist sub folder.
+ *
+ * \param batch The batch size (0 = default from parameters)
+ * \param parameters The parameters of the generator
+ * \return a unique_ptr around the create generator
+ */
 template<typename... Parameters>
 auto make_mnist_generator_train(size_t batch = 0, Parameters&&... parameters){
     return make_mnist_generator_train("mnist", batch, std::forward<Parameters>(parameters)...);
 }
 
+/*!
+ * \brief Create a data generator around the MNIST test set
+ *
+ * The MNIST train files are assumed to be in a mnist sub folder.
+ *
+ * \param batch The batch size (0 = default from parameters)
+ * \param parameters The parameters of the generator
+ * \return a unique_ptr around the create generator
+ */
 template<typename... Parameters>
 auto make_mnist_generator_test(size_t batch = 0, Parameters&&... parameters){
     return make_mnist_generator_test("mnist", batch, std::forward<Parameters>(parameters)...);
 }
 
+/*!
+ * \brief A dataset.
+ *
+ * A dataset is made of a train data generator and a test data generator.
+ */
 template<typename TrainG, typename TestG>
 struct dataset_holder {
-    std::unique_ptr<TrainG> train_generator;
-    std::unique_ptr<TestG> test_generator;
+private:
+    std::unique_ptr<TrainG> train_generator; ///< The train data generator
+    std::unique_ptr<TestG> test_generator;   ///< The test data generator
 
+public:
+    /*!
+     * \brief Construct a new dataset_holder
+     * \param train_generator The train data generator
+     * \param test_generator The test data generator
+     */
     dataset_holder(std::unique_ptr<TrainG>& train_generator, std::unique_ptr<TestG>& test_generator)
             : train_generator(std::move(train_generator)), test_generator(std::move(test_generator)) {
         // Nothing else to init
     }
 
+    /*!
+     * \brief Returns the generator around the train data
+     * \return A reference to the generator for the train data
+     */
     TrainG& train(){
         return *train_generator;
     }
 
+    /*!
+     * \brief Returns the generator around the test data
+     * \return A reference to the generator for the test data
+     */
     TestG& test(){
         return *test_generator;
     }
 
+    /*!
+     * \brief Display information about the dataset on the given stream
+     * \param stream The stream to output information to
+     * \return stream
+     */
     std::ostream& display(std::ostream& stream){
         std::cout << "MNIST Dataset" << std::endl;
 
@@ -111,33 +168,65 @@ struct dataset_holder {
         return stream;
     }
 
+    /*!
+     * \brief Display information about the dataset on the standard output
+     */
     void display(){
         display(std::cout);
     }
 };
 
-template<typename TrainG, typename TestG>
-dataset_holder<TrainG, TestG> make_dataset_holder(std::unique_ptr<TrainG>&& train_generator, std::unique_ptr<TestG>&& test_generator){
-    return {train_generator, test_generator};
-}
-
+/*!
+ * \brief Prints information about the dataset on the given stream
+ * \param os The output stream
+ * \param dataset The dataset to print information from
+ * \return os
+ */
 template<typename TrainG, typename TestG>
 std::ostream& operator<<(std::ostream& os, dataset_holder<TrainG, TestG>& dataset){
     return dataset.display(os);
 }
 
-template<typename... Parameters>
-auto make_mnist_dataset(size_t batch = 0, Parameters&&... parameters){
-    return make_dataset_holder(
-        make_mnist_generator_train(batch, std::forward<Parameters>(parameters)...),
-        make_mnist_generator_test(batch, std::forward<Parameters>(parameters)...));
+/*!
+ * \brief Helper to create a dataset_holder
+ * \param train_generator The train data generator
+ * \param test_generator The test data generator
+ * \return The dataset holder around the two generators
+ */
+template<typename TrainG, typename TestG>
+dataset_holder<TrainG, TestG> make_dataset_holder(std::unique_ptr<TrainG>&& train_generator, std::unique_ptr<TestG>&& test_generator){
+    return {train_generator, test_generator};
 }
 
+/*!
+ * \brief Creates a dataset around MNIST
+ * \param folder The folder in which the MNIST files are
+ * \param batch The batch size (0 = default from parameters)
+ * \param parameters The parameters of the generator
+ * \return The MNIST dataset
+ */
 template<typename... Parameters>
 auto make_mnist_dataset(const std::string& folder, size_t batch = 0, Parameters&&... parameters){
     return make_dataset_holder(
         make_mnist_generator_train(folder, batch, std::forward<Parameters>(parameters)...),
         make_mnist_generator_test(folder, batch, std::forward<Parameters>(parameters)...));
+}
+
+
+/*!
+ * \brief Creates a dataset around MNIST
+ *
+ * The MNIST train files are assumed to be in a mnist sub folder.
+ *
+ * \param batch The batch size (0 = default from parameters)
+ * \param parameters The parameters of the generator
+ * \return The MNIST dataset
+ */
+template<typename... Parameters>
+auto make_mnist_dataset(size_t batch = 0, Parameters&&... parameters){
+    return make_dataset_holder(
+        make_mnist_generator_train(batch, std::forward<Parameters>(parameters)...),
+        make_mnist_generator_test(batch, std::forward<Parameters>(parameters)...));
 }
 
 } // end of namespace dll
