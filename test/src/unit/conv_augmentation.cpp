f9623ebbbf8dd33a565edb62ab3ff0b9175df651
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-06-08 09:40:27 +0200

    Stabilize test case

--- test/src/unit/conv_augmentation.cpp
+++ test/src/unit/conv_augmentation.cpp
@@ -226,17 +226,17 @@ TEST_CASE("unit/augment/conv/mnist/5", "[dbn][unit]") {
 TEST_CASE("unit/augment/conv/mnist/6", "[dbn][unit]") {
     typedef dll::dbn_desc<
         dll::dbn_layers<
-            dll::conv_rbm_desc<1, 28, 28, 6, 3, 3, dll::momentum, dll::batch_size<10>>::layer_t,
-            dll::conv_rbm_desc<6, 26, 26, 6, 3, 3, dll::momentum, dll::batch_size<10>>::layer_t,
-            dll::mp_layer_2d_desc<6, 24, 24, 2, 2>::layer_t,
-            dll::rbm_desc<6 * 12 * 12, 300, dll::momentum, dll::batch_size<10>>::layer_t,
-            dll::rbm_desc<300, 10, dll::momentum, dll::batch_size<10>, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>,
+            dll::conv_rbm_desc<1, 28, 28, 6, 3, 3, dll::momentum, dll::batch_size<20>>::layer_t,
+            dll::conv_rbm_desc<6, 26, 26, 4, 3, 3, dll::momentum, dll::batch_size<20>>::layer_t,
+            dll::mp_layer_2d_desc<4, 24, 24, 2, 2>::layer_t,
+            dll::rbm_desc<4 * 12 * 12, 300, dll::momentum, dll::batch_size<20>>::layer_t,
+            dll::rbm_desc<300, 10, dll::momentum, dll::batch_size<20>, dll::hidden<dll::unit_type::SOFTMAX>>::layer_t>,
         dll::batch_size<25>, dll::momentum>::dbn_t dbn_t;
 
-    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(400);
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
     REQUIRE(!dataset.training_images.empty());
 
-    using pretrain_generator_t = dll::inmemory_data_generator_desc<dll::batch_size<10>, dll::noise<20>, dll::autoencoder, dll::binarize_pre<30>>;
+    using pretrain_generator_t = dll::inmemory_data_generator_desc<dll::batch_size<20>, dll::noise<20>, dll::autoencoder, dll::binarize_pre<30>>;
     using train_generator_t = dll::inmemory_data_generator_desc<dll::batch_size<25>, dll::noise<20>, dll::categorical, dll::binarize_pre<30>>;
 
     auto pretrain_generator = dll::make_generator(
