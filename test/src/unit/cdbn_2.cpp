5f041b68b8988463d99baef9b9f085618179746c
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-07-16 11:18:24 +0200

    Stabilize the test

--- test/src/unit/cdbn_2.cpp
+++ test/src/unit/cdbn_2.cpp
@@ -24,10 +24,10 @@
 TEST_CASE("unit/cdbn/mnist/7", "[cdbn][ap][svm][unit]") {
     typedef dll::dbn_desc<
         dll::dbn_layers<
-            dll::conv_rbm_desc<1, 28, 28, 20, 9, 8, dll::momentum, dll::batch_size<25>>::layer_t,
-            dll::avgp_layer_3d_desc<20, 20, 21, 2, 2, 3>::layer_t,
-            dll::conv_rbm_desc<10, 10, 7, 20, 3, 3, dll::momentum, dll::batch_size<25>>::layer_t,
-            dll::avgp_layer_3d_desc<20, 8, 5, 2, 1, 1>::layer_t>>::dbn_t dbn_t;
+            dll::conv_rbm_desc<1, 28, 28, 16, 9, 9, dll::momentum, dll::batch_size<25>>::layer_t,
+            dll::avgp_layer_3d_desc<16, 20, 20, 1, 2, 2>::layer_t,
+            dll::conv_rbm_desc<16, 10, 10, 8, 3, 3, dll::momentum, dll::batch_size<25>>::layer_t,
+            dll::avgp_layer_3d_desc<8, 8, 8, 1, 2, 2>::layer_t>>::dbn_t dbn_t;
 
     auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(200);
     REQUIRE(!dataset.training_images.empty());
@@ -36,12 +36,12 @@ TEST_CASE("unit/cdbn/mnist/7", "[cdbn][ap][svm][unit]") {
 
     auto dbn = std::make_unique<dbn_t>();
 
-    REQUIRE(dbn->output_size() == 400);
+    REQUIRE(dbn->output_size() == 8 * 4 * 4);
 
-    dbn->pretrain(dataset.training_images, 20);
+    dbn->pretrain(dataset.training_images, 25);
 
     auto output = dbn->activation_probabilities(dataset.training_images.front());
-    REQUIRE(output.size() == 400);
+    REQUIRE(output.size() == 8 * 4 * 4);
 
     auto result = dbn->svm_train(dataset.training_images, dataset.training_labels);
 
