df1c804a8c24f9e40ae7d7e1bb82673cc73a54ac
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-07-20 14:38:28 +0200

    Test He initialization

--- test/src/unit/conv_1.cpp
+++ test/src/unit/conv_1.cpp
@@ -97,13 +97,13 @@ TEST_CASE("unit/conv/sgd/4", "[unit][conv][dbn][mnist][sgd]") {
 TEST_CASE("unit/conv/sgd/5", "[conv][dbn][mnist][sgd]") {
     typedef dll::dbn_desc<
         dll::dbn_layers<
-            dll::conv_desc<1, 28, 28, 6, 5, 5, dll::activation<dll::function::RELU>>::layer_t,
-            dll::conv_desc<6, 24, 24, 4, 5, 5, dll::activation<dll::function::RELU>>::layer_t,
-            dll::dense_desc<4 * 20 * 20, 200, dll::activation<dll::function::RELU>>::layer_t,
+            dll::conv_desc<1, 28, 28, 6, 5, 5, dll::activation<dll::function::RELU>, dll::initializer<dll::initializer_type::HE>>::layer_t,
+            dll::conv_desc<6, 24, 24, 4, 5, 5, dll::activation<dll::function::RELU>, dll::initializer<dll::initializer_type::HE>>::layer_t,
+            dll::dense_desc<4 * 20 * 20, 200, dll::activation<dll::function::RELU>, dll::initializer<dll::initializer_type::HE>>::layer_t,
             dll::dense_desc<200, 10, dll::activation<dll::function::SOFTMAX>>::layer_t>,
         dll::trainer<dll::sgd_trainer>, dll::batch_size<20>>::dbn_t dbn_t;
 
-    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(500);
+    auto dataset = mnist::read_dataset_direct<std::vector, etl::fast_dyn_matrix<float, 1, 28, 28>>(1000);
     REQUIRE(!dataset.training_images.empty());
 
     dll_test::mnist_scale(dataset);
