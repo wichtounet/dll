d2a1dc007f3801d22dae10ddc48667a06c9abcd3
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-07-22 18:08:00 +0200

    Adapt a test

--- test/src/unit/bn.cpp
+++ test/src/unit/bn.cpp
@@ -14,7 +14,7 @@
 #include "dll/neural/activation_layer.hpp"
 #include "dll/neural/batch_normalization_layer.hpp"
 #include "dll/pooling/mp_layer.hpp"
-#include "dll/dbn.hpp"
+#include "dll/network.hpp"
 #include "dll/datasets.hpp"
 
 #include "mnist/mnist_reader.hpp"
@@ -22,8 +22,8 @@
 
 // (Dense) BN after the non-linearity
 TEST_CASE("unit/bn/1", "[unit][bn]") {
-    using network_t = dll::dbn_desc<
-        dll::dbn_layers<
+    using network_t = dll::network_desc<
+        dll::network_layers<
             dll::dense_desc<28 * 28, 200, dll::no_bias, dll::activation<dll::function::SIGMOID>>::layer_t,
             dll::batch_normalization_layer_2d_desc<200>::layer_t,
 
@@ -32,27 +32,27 @@ TEST_CASE("unit/bn/1", "[unit][bn]") {
 
             dll::dense_desc<200, 10, dll::activation<dll::function::SOFTMAX>>::layer_t
         >,
-        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::dbn_t;
+        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::network_t;
 
     auto dataset = dll::make_mnist_dataset_val(0, 1000, 2000, 0, dll::batch_size<25>{}, dll::scale_pre<255>{});
 
-    auto dbn = std::make_unique<network_t>();
+    auto net = std::make_unique<network_t>();
 
-    dbn->display();
+    net->display();
     dataset.display();
 
-    dbn->initial_momentum = 0.9;
-    dbn->final_momentum   = 0.9;
-    dbn->learning_rate    = 0.01;
+    net->initial_momentum = 0.9;
+    net->final_momentum   = 0.9;
+    net->learning_rate    = 0.01;
 
-    FT_CHECK_DATASET_VAL(50, 5e-2);
-    TEST_CHECK_DATASET(0.25);
+    FT_CHECK_2_VAL(net, dataset, 50, 5e-2);
+    TEST_CHECK_2(net, dataset, 0.25);
 }
 
 // (Dense) BN before the non-linearity
 TEST_CASE("unit/bn/2", "[unit][bn]") {
-    using network_t = dll::dbn_desc<
-        dll::dbn_layers<
+    using network_t = dll::network_desc<
+        dll::network_layers<
             dll::dense_desc<28 * 28, 200, dll::no_bias, dll::no_activation>::layer_t,
             dll::batch_normalization_layer_2d_desc<200>::layer_t,
             dll::activation_layer_desc<dll::function::SIGMOID>::layer_t,
@@ -64,27 +64,27 @@ TEST_CASE("unit/bn/2", "[unit][bn]") {
             dll::dense_desc<200, 10, dll::no_activation>::layer_t,
             dll::activation_layer_desc<dll::function::SOFTMAX>::layer_t
         >,
-        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::dbn_t;
+        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::network_t;
 
     auto dataset = dll::make_mnist_dataset_val(0, 1000, 3000, 0, dll::batch_size<25>{}, dll::scale_pre<255>{});
 
-    auto dbn = std::make_unique<network_t>();
+    auto net = std::make_unique<network_t>();
 
-    dbn->display();
+    net->display();
     dataset.display();
 
-    dbn->initial_momentum = 0.9;
-    dbn->final_momentum   = 0.9;
-    dbn->learning_rate    = 0.01;
+    net->initial_momentum = 0.9;
+    net->final_momentum   = 0.9;
+    net->learning_rate    = 0.01;
 
-    FT_CHECK_DATASET_VAL(50, 5e-2);
-    TEST_CHECK_DATASET(0.25);
+    FT_CHECK_2_VAL(net, dataset, 50, 5e-2);
+    TEST_CHECK_2(net, dataset, 0.25);
 }
 
 // (Conv) BN after the non-linearity
 TEST_CASE("unit/bn/3", "[unit][bn]") {
-    using network_t = dll::dbn_desc<
-        dll::dbn_layers<
+    using network_t = dll::network_desc<
+        dll::network_layers<
             dll::conv_desc<1, 28, 28, 6, 5, 5, dll::no_bias, dll::no_activation>::layer_t,
             dll::activation_layer_desc<dll::function::SIGMOID>::layer_t,
             dll::batch_normalization_layer_4d_desc<6, 24, 24>::layer_t,
@@ -104,27 +104,27 @@ TEST_CASE("unit/bn/3", "[unit][bn]") {
             dll::dense_desc<200, 10, dll::no_activation>::layer_t,
             dll::activation_layer_desc<dll::function::SOFTMAX>::layer_t
         >,
-        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::dbn_t;
+        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::network_t;
 
     auto dataset = dll::make_mnist_dataset_val(0, 500, 2500, 0, dll::batch_size<25>{}, dll::scale_pre<255>{});
 
-    auto dbn = std::make_unique<network_t>();
+    auto net = std::make_unique<network_t>();
 
-    dbn->display();
+    net->display();
     dataset.display();
 
-    dbn->initial_momentum = 0.9;
-    dbn->final_momentum   = 0.9;
-    dbn->learning_rate    = 0.01;
+    net->initial_momentum = 0.9;
+    net->final_momentum   = 0.9;
+    net->learning_rate    = 0.01;
 
-    FT_CHECK_DATASET_VAL(50, 5e-2);
-    TEST_CHECK_DATASET(0.25);
+    FT_CHECK_2_VAL(net, dataset, 50, 5e-2);
+    TEST_CHECK_2(net, dataset, 0.25);
 }
 
 // (Conv) BN before the non-linearity
 TEST_CASE("unit/bn/4", "[unit][bn]") {
-    using network_t = dll::dbn_desc<
-        dll::dbn_layers<
+    using network_t = dll::network_desc<
+        dll::network_layers<
             dll::conv_desc<1, 28, 28, 6, 5, 5, dll::no_bias, dll::no_activation>::layer_t,
             dll::batch_normalization_layer_4d_desc<6, 24, 24>::layer_t,
             dll::activation_layer_desc<dll::function::SIGMOID>::layer_t,
@@ -144,29 +144,29 @@ TEST_CASE("unit/bn/4", "[unit][bn]") {
             dll::dense_desc<200, 10, dll::no_activation>::layer_t,
             dll::activation_layer_desc<dll::function::SOFTMAX>::layer_t
         >,
-        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::dbn_t;
+        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::network_t;
 
     auto dataset = dll::make_mnist_dataset_val(0, 500, 2500, 0, dll::batch_size<25>{}, dll::scale_pre<255>{});
 
-    auto dbn = std::make_unique<network_t>();
+    auto net = std::make_unique<network_t>();
 
-    dbn->display();
+    net->display();
     dataset.display();
 
-    dbn->initial_momentum = 0.9;
-    dbn->final_momentum   = 0.9;
-    dbn->learning_rate    = 0.01;
+    net->initial_momentum = 0.9;
+    net->final_momentum   = 0.9;
+    net->learning_rate    = 0.01;
 
-    FT_CHECK_DATASET_VAL(50, 5e-2);
-    TEST_CHECK_DATASET(0.25);
+    FT_CHECK_2_VAL(net, dataset, 50, 5e-2);
+    TEST_CHECK_2(net, dataset, 0.25);
 }
 
 // (Conv+MP) BN after the non-linearity
 TEST_CASE("unit/bn/5", "[unit][bn]") {
     constexpr size_t K = 6;
 
-    using network_t = dll::dbn_desc<
-        dll::dbn_layers<
+    using network_t = dll::network_desc<
+        dll::network_layers<
             dll::conv_desc<1, 28, 28, K, 5, 5, dll::no_bias, dll::no_activation>::layer_t,
             dll::activation_layer_desc<dll::function::SIGMOID>::layer_t,
             dll::mp_layer_2d_desc<K, 24, 24, 2, 2>::layer_t,
@@ -188,19 +188,19 @@ TEST_CASE("unit/bn/5", "[unit][bn]") {
             dll::dense_desc<200, 10, dll::no_activation>::layer_t,
             dll::activation_layer_desc<dll::function::SOFTMAX>::layer_t
         >,
-        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::dbn_t;
+        dll::updater<dll::updater_type::ADADELTA>, dll::early_training, dll::batch_size<25>>::network_t;
 
     auto dataset = dll::make_mnist_dataset_val(0, 500, 2500, 0, dll::batch_size<25>{}, dll::scale_pre<255>{});
 
-    auto dbn = std::make_unique<network_t>();
+    auto net = std::make_unique<network_t>();
 
-    dbn->display();
+    net->display();
     dataset.display();
 
-    dbn->initial_momentum = 0.9;
-    dbn->final_momentum   = 0.9;
-    dbn->learning_rate    = 0.01;
+    net->initial_momentum = 0.9;
+    net->final_momentum   = 0.9;
+    net->learning_rate    = 0.01;
 
-    FT_CHECK_DATASET_VAL(50, 5e-2);
-    TEST_CHECK_DATASET(0.25);
+    FT_CHECK_2_VAL(net, dataset, 50, 5e-2);
+    TEST_CHECK_2(net, dataset, 0.25);
 }
