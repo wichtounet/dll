740e0381911cdf7481febe3e0365dfba00ab338f
Yves <43539045+yvesfasel@users.noreply.github.com>
2020-05-03 14:41:55 +0200

    changes for arm compability

--- include/dll/layer.hpp
+++ include/dll/layer.hpp
@@ -48,10 +48,15 @@ struct layer {
      * \brief Default initialize the layer
      */
     layer() {
+#ifdef __ARM_ARCH
+// do not set flush zero mode
+#else
 #ifndef DLL_DENORMALS
         // Disable denormals for performance reason
-        _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+          _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 #endif
+#endif
+
     }
 
     /*!
