c58f959f1c224c9f0e1b05fde8d13adcaced245d
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-05-16 13:18:31 +0200

    Update Copyright to 2020

--- test/src/misc/dyn_crbm_mp.cpp
+++ test/src/misc/dyn_crbm_mp.cpp
@@ -1,5 +1,5 @@
 //=======================================================================
-// Copyright (c) 2014-2017 Baptiste Wicht
+// Copyright (c) 2014-2020 Baptiste Wicht
 // Distributed under the terms of the MIT License.
 // (See accompanying file LICENSE or copy at
 //  http://opensource.org/licenses/MIT)
