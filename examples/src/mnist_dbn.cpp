935dd28b1cd6e24badefcc0b9d2f052d8b755339
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-05-22 13:50:50 +0200

    Unify examples

--- examples/src/mnist_dbn.cpp
+++ examples/src/mnist_dbn.cpp
@@ -11,18 +11,19 @@
 
 int main(int /*argc*/, char* /*argv*/ []) {
     // Load the dataset
-    auto ae_dataset = dll::make_mnist_ae_dataset(dll::batch_size<50>{}, dll::binarize_pre<30>{});
-    auto dataset = dll::make_mnist_dataset(dll::batch_size<50>{}, dll::binarize_pre<30>{});
+    auto ae_dataset = dll::make_mnist_ae_dataset(dll::batch_size<100>{}, dll::binarize_pre<30>{});
+    auto dataset = dll::make_mnist_dataset(dll::batch_size<100>{}, dll::binarize_pre<30>{});
 
     // Build the network
 
     using network_t = dll::network_desc<
         dll::network_layers<
-            dll::rbm<28 * 28, 500, dll::batch_size<50>, dll::momentum>,
-            dll::rbm<500, 250, dll::batch_size<50>, dll::momentum>,
-            dll::rbm<250, 10, dll::batch_size<50>, dll::hidden<dll::unit_type::SOFTMAX>>
+            dll::rbm<28 * 28, 500, dll::batch_size<100>, dll::momentum>,
+            dll::rbm<500, 250, dll::batch_size<100>, dll::momentum>,
+            dll::rbm<250, 10, dll::batch_size<100>, dll::hidden<dll::unit_type::SOFTMAX>>
         >
-        , dll::batch_size<50>                       // The mini-batch size
+        , dll::updater<dll::updater_type::NADAM>     // Nesterov Adam (NADAM)
+        , dll::batch_size<100>                       // The mini-batch size
         , dll::shuffle                               // Shuffle before each epoch
     >::network_t;
 
