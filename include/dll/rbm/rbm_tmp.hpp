c50c95945a6395d439bcbfffb3046c2691f0f943
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-05-23 15:44:05 +0200

    Remove old macros

--- include/dll/rbm/rbm_tmp.hpp
+++ include/dll/rbm/rbm_tmp.hpp
@@ -8,18 +8,3 @@
 #pragma once
 
 #include "cpp_utils/tmp.hpp"
-
-#define H_PROBS(unit, ...) if constexpr (P && hidden_unit == unit) { __VA_ARGS__; }
-#define H_PROBS2(hunit, vunit, ...) if constexpr (P && hidden_unit == hunit && visible_unit == vunit) { __VA_ARGS__; }
-#define H_PROBS_MULTI(unit) if constexpr (P && hidden_unit == unit)
-#define H_SAMPLE_INPUT(unit, ...) if constexpr (!P && S && hidden_unit == unit) { __VA_ARGS__; }
-#define H_SAMPLE_INPUT_MULTI(unit) if constexpr (!P && S && hidden_unit == unit)
-#define H_SAMPLE_PROBS(unit, ...) if constexpr (P && S && hidden_unit == unit) { __VA_ARGS__; }
-#define H_SAMPLE_PROBS_MULTI(unit) if constexpr (P && S && hidden_unit == unit)
-
-#define V_PROBS(unit, ...) if constexpr (P && visible_unit == unit) { __VA_ARGS__; }
-#define V_PROBS_MULTI(unit) if constexpr (P && visible_unit == unit)
-#define V_SAMPLE_INPUT(unit, ...) if constexpr (!P && S && visible_unit == unit) { __VA_ARGS__; }
-#define V_SAMPLE_INPUT_MULTI(unit) if constexpr (!P && S && visible_unit == unit)
-#define V_SAMPLE_PROBS(unit, ...) if constexpr (P && S && visible_unit == unit) { __VA_ARGS__; }
-#define V_SAMPLE_PROBS_MULTI(unit) if constexpr (P && S && visible_unit == unit)
